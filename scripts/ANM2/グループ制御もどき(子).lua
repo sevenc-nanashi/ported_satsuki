3e940777bdcb62cfb07ab4399da1979402613bc0
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-01 21:44:00 +0900

    feat: グループ制御もどきを改善

--- scripts/ANM2/グループ制御もどき(子).lua
+++ scripts/ANM2/グループ制御もどき(子).lua
@@ -1,25 +1,34 @@
 --label:${ROOT_CATEGORY}\オブジェクト制御
+
+local x = S_GRP_x
+local y = S_GRP_y
+local z = S_GRP_z
+local zoom = S_GRP_zoom
+local alpha = S_GRP_alpha
+local r = S_GRP_r
+local jiku = S_GRP_jiku
+
 if jiku < 1 then
-	l_rz = math.sqrt(obj.getvalue("x") ^ 2 + obj.getvalue("y") ^ 2) * zoom
-	r_rz = math.atan2(obj.getvalue("y"), obj.getvalue("x"))
-	obj.ox = obj.ox + x + l_rz * (math.cos(r * math.pi / 180 + r_rz)) - obj.getvalue("x")
-	obj.oy = obj.oy + y + l_rz * (math.sin(r * math.pi / 180 + r_rz)) - obj.getvalue("y")
-	obj.oz = obj.oz + z + (zoom - 1) * obj.getvalue("z")
-	obj.rz = r
+  local l_rz = math.sqrt(obj.getvalue("x") ^ 2 + obj.getvalue("y") ^ 2) * zoom
+  local r_rz = math.atan2(obj.getvalue("y"), obj.getvalue("x"))
+  obj.ox = obj.ox + x + l_rz * (math.cos(r * math.pi / 180 + r_rz)) - obj.getvalue("x")
+  obj.oy = obj.oy + y + l_rz * (math.sin(r * math.pi / 180 + r_rz)) - obj.getvalue("y")
+  obj.oz = obj.oz + z + (zoom - 1) * obj.getvalue("z")
+  obj.rz = r
 elseif jiku < 2 then
-	l_ry = math.sqrt(obj.getvalue("x") ^ 2 + obj.getvalue("z") ^ 2) * zoom
-	r_ry = math.atan2(obj.getvalue("z"), obj.getvalue("x"))
-	obj.ox = obj.ox + x + l_ry * (math.cos(-r * math.pi / 180 + r_ry)) - obj.getvalue("x")
-	obj.oy = obj.oy + y + (zoom - 1) * obj.getvalue("y")
-	obj.oz = obj.oz + z + l_ry * (math.sin(-r * math.pi / 180 + r_ry)) - obj.getvalue("z")
-	obj.ry = r
+  local l_ry = math.sqrt(obj.getvalue("x") ^ 2 + obj.getvalue("z") ^ 2) * zoom
+  local r_ry = math.atan2(obj.getvalue("z"), obj.getvalue("x"))
+  obj.ox = obj.ox + x + l_ry * (math.cos(-r * math.pi / 180 + r_ry)) - obj.getvalue("x")
+  obj.oy = obj.oy + y + (zoom - 1) * obj.getvalue("y")
+  obj.oz = obj.oz + z + l_ry * (math.sin(-r * math.pi / 180 + r_ry)) - obj.getvalue("z")
+  obj.ry = r
 else
-	l_rx = math.sqrt(obj.getvalue("y") ^ 2 + obj.getvalue("z") ^ 2) * zoom
-	r_rx = math.atan2(obj.getvalue("z"), obj.getvalue("y"))
-	obj.ox = obj.ox + x + (zoom - 1) * obj.getvalue("x")
-	obj.oy = obj.oy + y + l_rx * (math.cos(r * math.pi / 180 + r_rx)) - obj.getvalue("y")
-	obj.oz = obj.oz + z + l_rx * (math.sin(r * math.pi / 180 + r_rx)) - obj.getvalue("z")
-	obj.rx = r
+  local l_rx = math.sqrt(obj.getvalue("y") ^ 2 + obj.getvalue("z") ^ 2) * zoom
+  local r_rx = math.atan2(obj.getvalue("z"), obj.getvalue("y"))
+  obj.ox = obj.ox + x + (zoom - 1) * obj.getvalue("x")
+  obj.oy = obj.oy + y + l_rx * (math.cos(r * math.pi / 180 + r_rx)) - obj.getvalue("y")
+  obj.oz = obj.oz + z + l_rx * (math.sin(r * math.pi / 180 + r_rx)) - obj.getvalue("z")
+  obj.rx = r
 end
 obj.zoom = zoom
 obj.alpha = alpha
--- scripts/ANM2/グループ制御もどき(親).lua
+++ scripts/ANM2/グループ制御もどき(親).lua
@@ -2,21 +2,22 @@
 ---$track:透明度2
 ---min=0
 ---max=100
-local track0 = 0
+local transparency2 = 0
 ---$track:角度
 ---min=-720
 ---max=720
-local track1 = 0
----$track:回転軸
----min=0
----max=2
----step=1
+local angle = 0
+---$select:回転軸
+---X=0
+---Y=1
+---Z=2
 local jiku = 0
 obj.load("figure", "四角形", 0xffffff, 1)
 obj.drawpoly(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
-x = obj.getvalue("x")
-y = obj.getvalue("y")
-z = obj.getvalue("z")
-zoom = obj.getvalue("zoom") / 100
-alpha = 1 - obj.getvalue(0) / 100
-r = obj.getvalue(1)
+S_GRP_x = obj.getvalue("x")
+S_GRP_y = obj.getvalue("y")
+S_GRP_z = obj.getvalue("z")
+S_GRP_zoom = obj.getvalue("zoom") / 100
+S_GRP_alpha = 1 - transparency2 / 100
+S_GRP_r = angle
+S_GRP_jiku = jiku
