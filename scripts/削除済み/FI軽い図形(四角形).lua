2cd4255b185ff5b1e8ae979b3040103332da6a72
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-03 23:17:49 +0900

    feat: FI軽い図形を動作確認

--- scripts/削除済み/FI軽い図形(四角形).lua
+++ scripts/削除済み/FI軽い図形(四角形).lua
@@ -2,27 +2,27 @@
 ---$track:X幅
 ---min=0
 ---max=20000
-local track0 = 100
+local width = 100
 ---$track:Y幅
 ---min=0
 ---max=20000
-local track1 = 100
+local height = 100
 ---$track:X中心%
 ---min=-100
 ---max=100
-local track2 = 0
+local center_x = 0
 ---$track:Y中心%
 ---min=-100
 ---max=100
-local track3 = 0
+local center_y = 0
 ---$color:色
 local color = 0xffffff
 
 obj.load("figure", "四角形", color, 2)
-sx = track0 / 2
-sy = track1 / 2
-cx = track2 / 100
-cy = track3 / 100
+local sx = width / 2
+local sy = height / 2
+local cx = center_x / 100
+local cy = center_y / 100
 obj.drawpoly(
 	-sx * (1 + cx),
 	-sy * (1 + cy),
