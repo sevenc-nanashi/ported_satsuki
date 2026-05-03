106ce832e0987604e783caecd8cd9a66c283dd22
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-03 23:16:55 +0900

    feat: FIランダム輪出現を動作確認

--- scripts/削除済み/FIランダムに輪が出現2.lua
+++ scripts/削除済み/FIランダムに輪が出現2.lua
@@ -2,30 +2,30 @@
 ---$track:時間[s]
 ---min=0
 ---max=10
-local track0 = 2
+local duration = 2
 ---$track:間隔[s]
 ---min=0.1
 ---max=10
-local track1 = 1
+local interval = 1
 ---$track:サイズ
 ---min=0
 ---max=1000
-local track2 = 500
+local size = 500
 ---$track:ライン幅
 ---min=0
 ---max=1000
-local track3 = 10
+local weight = 10
 ---$figure:種類
 local name = "円"
 
-n = obj.totaltime / track1
+local n = obj.totaltime / interval
 for i = 0, n do
-	ra = obj.rand(0, 255, 0, i)
-	rb = obj.rand(0, 255, 1, i)
-	rc = obj.rand(0, 255, 2, i)
-	color = string.format("0x%02x%02x%02x", ra, rb, rc)
-	obj.load("figure", name, color, track2, track3)
-	t = (track0 - obj.time + i * track1) / track0
+	local ra = obj.rand(0, 255, 0, i)
+	local rb = obj.rand(0, 255, 1, i)
+	local rc = obj.rand(0, 255, 2, i)
+	local color = RGB(ra, rb, rc)
+	obj.load("figure", name, color, size, weight)
+	local t = (duration - obj.time + i * interval) / duration
 	if t > 0 then
 		if t > 1 then
 			obj.alpha = 0
@@ -34,7 +34,7 @@ for i = 0, n do
 	else
 		t = 0
 	end
-	x = rand(-obj.screen_w / 2, obj.screen_w / 2, i, 0)
-	y = rand(-obj.screen_h / 2, obj.screen_h / 2, i, 0)
+	local x = rand(-obj.screen_w / 2, obj.screen_w / 2, i, 0)
+	local y = rand(-obj.screen_h / 2, obj.screen_h / 2, i, 0)
 	obj.draw(x, y, 0, 1 - t, t)
 end
