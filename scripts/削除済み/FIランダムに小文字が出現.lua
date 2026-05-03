42e7b36a45f193f965fa5f401f1fecf55221f2a9
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-03 23:13:06 +0900

    feat: FIランダム文字出現を動作確認

--- scripts/削除済み/FIランダムに小文字が出現.lua
+++ scripts/削除済み/FIランダムに小文字が出現.lua
@@ -2,16 +2,16 @@
 ---$track:時間[s]
 ---min=0
 ---max=10
-local track0 = 2
+local duration = 2
 ---$track:間隔[s]
 ---min=0.1
 ---max=10
-local track1 = 1
+local interval = 1
 ---$track:サイズ
 ---min=0
 ---max=256
-local track2 = 100
----$value:フォント
+local size = 100
+---$font:フォント
 local font = "MS UI Cothic"
 
 ---$color:文字色
@@ -20,10 +20,17 @@ local col1 = 0xffffff
 ---$color:影・縁色
 local col2 = 0x000000
 
----$value:装飾タイプ[0-4]
+---$select:装飾タイプ
+---標準文字=0
+---影付き文字=1
+---影付き文字（薄）=2
+---縁取り文字=3
+---縁取り文字（細）=4
+---縁取り文字（太）=5
+---縁取り文字（角）=6
 local type = 0
 
-word = {
+local word = {
 	"a",
 	"b",
 	"c",
@@ -51,11 +58,11 @@ word = {
 	"y",
 	"z",
 }
-n = obj.totaltime / track1
+local n = obj.totaltime / interval
 for i = 0, n do
-	obj.setfont(font, track2, type, col1, col2)
+	obj.setfont(font, size, type, col1, col2)
 	obj.load("text", word[obj.rand(1, #word, i, 0)])
-	t = (track0 - obj.time + i * track1) / track0
+	local t = (duration - obj.time + i * interval) / duration
 	if t > 0 then
 		if t > 1 then
 			obj.alpha = 0
@@ -64,7 +71,7 @@ for i = 0, n do
 	else
 		t = 0
 	end
-	x = rand(-obj.screen_w / 2, obj.screen_w / 2, i, 0)
-	y = rand(-obj.screen_h / 2, obj.screen_h / 2, i, 0)
+	local x = rand(-obj.screen_w / 2, obj.screen_w / 2, i, 0)
+	local y = rand(-obj.screen_h / 2, obj.screen_h / 2, i, 0)
 	obj.draw(x, y, 0, 1 - t, t, 0, 0, obj.rand(0, 360, 0, i))
 end
