8bf8f55c64da9503669b7894b414ea0c97c8b655
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-05 15:25:33 +0900

    feat: グリッドを移植

--- scripts/FIGURE/グリッド.lua
+++ scripts/FIGURE/グリッド.lua
@@ -2,36 +2,46 @@
 ---$track:ライン幅
 ---min=1
 ---max=100
+---step=1
 local l0 = 2
 ---$track:間隔
 ---min=1
 ---max=100
+---step=1
 local l1 = 10
 ---$track:X範囲
 ---min=0
 ---max=500
-local track2 = 100
+---step=1
+local x_range = 100
 ---$track:Y範囲
 ---min=0
 ---max=500
-local track3 = 100
+---step=1
+local y_range = 100
 ---$color:色
 local color = 0x0000ff
 
 ---$check:横線のみ
-local yoko = 0
+local yoko = false
+
+--[[pixelshader@grid:
+---$include "./shaders/grid.hlsl"
+]]
 
 obj.load("figure", "四角形", color, 2)
-w = obj.screen_w * track2 / 100
-h = obj.screen_h * track3 / 100
-n = math.floor(math.max(w, h) / (l0 + l1) / 2)
-obj.setoption("dst", "tmp", w, h)
-for i = -n - 1, n do
-	y0 = (l0 + l1) * i
-	y2 = l0 + (l0 + l1) * i
-	obj.drawpoly(-w / 2, y0, 0, w / 2, y0, 0, w / 2, y2, 0, -w / 2, y2, 0)
-	if yoko == 0 then
-		obj.drawpoly(y0, -h / 2, 0, y2, -h / 2, 0, y2, h / 2, 0, y0, h / 2, 0)
-	end
-end
+local w = obj.screen_w * x_range / 100
+local h = obj.screen_h * y_range / 100
+local period = l0 + l1
+local horizontal_only = yoko and 1 or 0
+
+obj.setoption("drawtarget", "tempbuffer", w, h)
+obj.drawpoly(-w / 2, -h / 2, 0, w / 2, -h / 2, 0, w / 2, h / 2, 0, -w / 2, h / 2, 0)
+obj.pixelshader("grid", "tempbuffer", "tempbuffer", {
+	l0,
+	period,
+	w,
+	h,
+	horizontal_only,
+})
 obj.load("tempbuffer")
