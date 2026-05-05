5561947326b4432bea7bd3754abb7e65e5cb4391
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-05 14:42:20 +0900

    feat: Twitchっぽいものを移植

--- scripts/ANM1/twitchっぽいもの.lua
+++ scripts/ANM1/twitchっぽいもの.lua
@@ -15,61 +15,82 @@ local h = 50
 ---min=0
 ---max=100
 local syu = 10
----$value:X振動
+---$track:X振動
+---min=-1000
+---max=1000
+---step=1
 local x = 50
 
----$value:Y振動
+---$track:Y振動
+---min=-1000
+---max=1000
+---step=1
 local y = 50
 
----$value:Z振動
+---$track:Z振動
+---min=-1000
+---max=1000
+---step=1
 local z = 50
 
----$value:揺れ幅倍率
+---$track:揺れ幅倍率
+---min=0
+---max=10
+---step=0.1
 local haba = 3
 
----$value:色ずれ幅
+---$track:色ずれ幅
+---min=0
+---max=1000
+---step=1
 local irozure = 100
 
----$value:色ずれ種類[0〜2]
+---$select:色ずれ種類
+---赤緑A=0
+---赤青A=1
+---緑青A=2
+---赤緑B=3
+---赤青B=4
+---緑青B=5
 local ty = 0
 
 obj.effect(
-	"振動",
-	"X",
-	x,
-	"Y",
-	y,
-	"Z",
-	z,
-	"周期",
-	syu,
-	"ランダムに強さを変える",
-	1,
-	"複雑に振動",
-	1
+  "振動",
+  "X",
+  x,
+  "Y",
+  y,
+  "Z",
+  z,
+  "周期",
+  syu,
+  "ランダムに強さを変える",
+  1,
+  "複雑に振動",
+  1
 )
 obj.effect(
-	"振動",
-	"X",
-	-haba * x,
-	"Y",
-	-haba * y,
-	"Z",
-	-haba * z,
-	"周期",
-	haba * syu,
-	"ランダムに強さを変える",
-	1,
-	"複雑に振動",
-	1
+  "振動",
+  "X",
+  -haba * x,
+  "Y",
+  -haba * y,
+  "Z",
+  -haba * z,
+  "周期",
+  haba * syu,
+  "ランダムに強さを変える",
+  1,
+  "複雑に振動",
+  1
 )
-s = rand(0, 100)
+local s = rand(0, 100)
 if s > siki then
-	x = obj.rand(-obj.screen_w / 2, obj.screen_w / 2)
-	y = obj.rand(-obj.screen_h / 2, obj.screen_h / 2)
-	a = obj.rand(0, t)
-	obj.effect("放射ブラー", "範囲", a, "X", x, "Y", y)
-	obj.effect("発光", "強さ", 100 + a * 4, "拡散", 800, "しきい値", h)
-	obj.effect("色ずれ", "ずれ幅", a / 2 * irozure / 100, "角度", obj.rand(0, 360), "type", ty)
-	obj.zoom = obj.zoom + a / 50
+  local dx = obj.rand(-obj.screen_w / 2, obj.screen_w / 2)
+  local dy = obj.rand(-obj.screen_h / 2, obj.screen_h / 2)
+  local a = obj.rand(0, t)
+  obj.effect("放射ブラー", "範囲", a, "X", dx, "Y", dy, "サイズ固定", 1)
+  obj.effect("発光", "強さ", 100 + a * 4, "拡散", 800, "しきい値", h, "サイズ固定", 1)
+  obj.effect("色ずれ", "ずれ幅", a / 2 * irozure / 100, "角度", obj.rand(0, 360), "色ずれの種類", ty)
+  obj.zoom = obj.zoom + a / 50
 end
