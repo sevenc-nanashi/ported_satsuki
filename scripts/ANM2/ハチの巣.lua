5ed617d71554937c30314cc885de9b1907677912
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-05 15:43:23 +0900

    feat: ハチの巣を移植

--- scripts/ANM2/ハチの巣.lua
+++ scripts/ANM2/ハチの巣.lua
@@ -18,20 +18,48 @@ local nx = 5
 ---step=1
 local ny = 3
 obj.effect()
-gx = track0 / 100 - (1 - math.cos(math.pi * 30 / 180))
-gy = track1 / 100 - (1 - math.sin(math.pi * 30 / 180) * 1.5)
-w = obj.w
-h = obj.h
+local gx = track0 / 100 - (1 - math.cos(math.pi * 30 / 180))
+local gy = track1 / 100 - (1 - math.sin(math.pi * 30 / 180) * 1.5)
+local w = obj.w
+local h = obj.h
+local base_x = -(nx + (nx - 1) * (gx - 1)) * w / 2
+local base_y = -(ny + (ny - 1) * (gy - 1)) * h / 2
+local vertices = {}
+
 for j = 0, ny - 1 do
-	y0 = -(ny + (ny - 1) * (gy - 1)) * h / 2 + h * j * gy
-	y2 = -(ny + (ny - 1) * (gy - 1)) * h / 2 + h * (j * gy + 1)
+	local y0 = base_y + h * j * gy
+	local y2 = y0 + h
+	local row_shift = 0
+	if j % 2 == 0 then
+		row_shift = w / 2 * gx
+	end
+
 	for i = 0, nx - 1 do
-		x0 = -(nx + (nx - 1) * (gx - 1)) * w / 2 + w * i * gx
-		x1 = -(nx + (nx - 1) * (gx - 1)) * w / 2 + w * (i * gx + 1)
-		if j % 2 == 0 then
-			x0 = x0 + w / 2 * gx
-			x1 = x1 + w / 2 * gx
-		end
-		obj.drawpoly(x0, y0, 0, x1, y0, 0, x1, y2, 0, x0, y2, 0, 0, 0, w, 0, w, h, 0, h)
+		local x0 = base_x + w * i * gx + row_shift
+		local x1 = x0 + w
+		vertices[#vertices + 1] = {
+			x0,
+			y0,
+			0,
+			x1,
+			y0,
+			0,
+			x1,
+			y2,
+			0,
+			x0,
+			y2,
+			0,
+			0,
+			0,
+			w,
+			0,
+			w,
+			h,
+			0,
+			h,
+		}
 	end
 end
+
+obj.drawpoly(vertices)
