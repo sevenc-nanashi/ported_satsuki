cb39d96263327317aeef48138612c22ed86e5776
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-05 14:48:47 +0900

    feat: きらりを移植

--- scripts/FIGURE/きらり.lua
+++ scripts/FIGURE/きらり.lua
@@ -2,23 +2,23 @@
 ---$track:サイズ
 ---min=1
 ---max=2000
-local s = 300
+local size = 300
 ---$track:幅
 ---min=0
 ---max=500
-local track1 = 100
+local width = 100
 ---$track:円透明
 ---min=0
 ---max=100
-local track2 = 70
+local circle_opacity = 70
 ---$color:色
 local color = 0xffffff
 
-obj.setoption("dst", "tmp", s, s)
-obj.load("figure", "三角形", color, s / 2)
-obj.effect("ぼかし", "範囲", s / 15, "縦横比", 100)
-w = s / 200 * track1 / 100
-h = s / 4 * 3 / 2
+obj.setoption("drawtarget", "tempbuffer", size, size)
+obj.load("figure", "三角形", color, size / 2)
+obj.effect("ぼかし", "範囲", size / 15, "縦横比", 100)
+local w = size / 200 * width / 100
+local h = size / 4 * 3 / 2
 obj.drawpoly(-w, -h, 0, w, -h, 0, w, h / 3, 0, -w, h / 3, 0)
 obj.drawpoly(h, -w, 0, h, w, 0, -h / 3, w, 0, -h / 3, -w, 0)
 obj.drawpoly(w, h, 0, -w, h, 0, -w, -h / 3, 0, w, -h / 3, 0)
@@ -28,11 +28,11 @@ obj.load("tempbuffer")
 obj.draw(0, 0, 0, 0.5, 1, 0, 0, 45)
 obj.alpha = 0
 
-obj.load("figure", "円", color, s / 3, s / 25)
-obj.effect("ぼかし", "範囲", s / 7.5)
-obj.draw(0, 0, 0, 1, track2 / 100)
+obj.load("figure", "円", color, size / 3, size / 25)
+obj.effect("ぼかし", "範囲", size / 7.5)
+obj.draw(0, 0, 0, 1, circle_opacity / 100)
 
-obj.load("figure", "円", color, s / 15)
-obj.effect("ぼかし", "範囲", s / 60)
+obj.load("figure", "円", color, size / 15)
+obj.effect("ぼかし", "範囲", size / 60)
 obj.draw()
 obj.load("tempbuffer")
