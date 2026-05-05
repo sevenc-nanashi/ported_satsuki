bc82b40b465687a8cc58c4fcf3c428bb5db7c3e1
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-05 15:04:53 +0900

    feat: ものまねを移植

--- scripts/削除済み/ものまね.lua
+++ scripts/削除済み/ものまね.lua
@@ -7,21 +7,33 @@ local lay = 1
 ---$track:時間差[ms]
 ---min=-5000
 ---max=5000
-local track1 = 0
----$value:X倍率
+local delay_ms = 0
+---$track:X倍率
+---min=-1000
+---max=1000
+---step=1
 local xx = 100
 
----$value:Y倍率
+---$track:Y倍率
+---min=-1000
+---max=1000
+---step=1
 local yy = 100
 
----$value:Z倍率
+---$track:Z倍率
+---min=-1000
+---max=1000
+---step=1
 local zz = 100
 
----$value:拡大倍率
+---$track:拡大倍率
+---min=0
+---max=1000
+---step=1
 local ss = 100
 
-t = track1 / 1000
-obj.ox = obj.getvalue("layer" .. lay .. ".x", obj.time + t) * xx / 100
-obj.oy = obj.getvalue("layer" .. lay .. ".y", obj.time + t) * yy / 100
-obj.oz = obj.getvalue("layer" .. lay .. ".z", obj.time + t) * zz / 100
-obj.zoom = obj.getvalue("layer" .. lay .. ".zoom", obj.time + t) / 100 * ss / 100
+local delay = delay_ms / 1000
+obj.ox = obj.getvalue("layer" .. lay .. ".x", obj.time + delay) * xx / 100
+obj.oy = obj.getvalue("layer" .. lay .. ".y", obj.time + delay) * yy / 100
+obj.oz = obj.getvalue("layer" .. lay .. ".z", obj.time + delay) * zz / 100
+obj.zoom = obj.getvalue("layer" .. lay .. ".zoom", obj.time + delay) / 100 * ss / 100
