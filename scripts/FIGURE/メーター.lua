8a124df1bd2f592a818dd4ad195be232a46fa56f
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-06 23:38:25 +0900

    feat: メーターを移植

--- scripts/FIGURE/メーター.lua
+++ scripts/FIGURE/メーター.lua
@@ -2,37 +2,37 @@
 ---$track:高さ
 ---min=0
 ---max=900
-local track0 = 5
+local height = 5
 ---$track:長さ
 ---min=0
 ---max=1600
-local track1 = 800
----$track:モード
----min=-1
----max=1
----step=1
+local width = 800
+---$select:モード
+---右→左=-1
+---左右→中央=0
+---左→右=1
 local track2 = 0
----$value:色1
+---$color:色1
 local col1 = 0x0000ff
 
----$value:色2
+---$color:色2
 local col2 = 0x00ffff
 
----$value:色3
+---$color:色3
 local col3 = 0x00ff00
 
----$value:色4
+---$color:色4
 local col4 = 0xffff00
 
----$value:色5
+---$color:色5
 local col5 = 0xff0000
 
-mcol = { col1, col2, col3, col4, col5, col5 }
-i = math.floor(obj.time / obj.totaltime * 5) + 1
+local mcol = { col1, col2, col3, col4, col5, col5 }
+local i = math.floor(obj.time / obj.totaltime * 5) + 1
 obj.load("figure", "四角形", mcol[i], 2)
-h = track0 / 2
-w0 = -track1 / 2 * (1 - obj.time / obj.totaltime * (1 + track2))
-w1 = track1 / 2 * (1 - obj.time / obj.totaltime * (1 - track2))
-obj.setoption("dst", "tmp", track1, track0)
+local h = height / 2
+local w0 = -width / 2 * (1 - obj.time / obj.totaltime * (1 + track2))
+local w1 = width / 2 * (1 - obj.time / obj.totaltime * (1 - track2))
+obj.setoption("drawtarget", "tempbuffer", width, height)
 obj.drawpoly(w0, -h, 0, w1, -h, 0, w1, h, 0, w0, h, 0)
 obj.load("tempbuffer")
