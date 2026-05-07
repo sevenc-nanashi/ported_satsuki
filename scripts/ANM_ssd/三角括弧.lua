2f7b9dad173bbc034160b8c2f2d1817e88bcd086
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-07 13:25:02 +0900

    feat: 三角括弧を動作確認

--- scripts/ANM_ssd/三角括弧.lua
+++ scripts/ANM_ssd/三角括弧.lua
@@ -3,37 +3,37 @@
 ---min=0
 ---max=1000
 ---step=1
-local w = 100
+local width = 100
 ---$track:角度
 ---min=1
 ---max=180
-local track1 = 120
+local height = 120
 ---$track:長さ
 ---min=0
 ---max=2000
-local l = 50
+local length = 50
 ---$track:移動
 ---min=-10000
 ---max=10000
-local track3 = 0
+local movement = 0
 ---$color:色
 local color = 0xffffff
 
-local r = math.rad(track1 / 2)
-local tl = l + w / 2 / math.tan(r)
+local r = math.rad(height / 2)
+local tl = length + width / 2 / math.tan(r)
 
 obj.setoption("blend", "alpha_add")
-obj.setoption("drawtarget", "tempbuffer", tl, w)
+obj.setoption("drawtarget", "tempbuffer", tl, width)
 
 obj.load("figure", "四角形", color, 2)
-obj.drawpoly(tl / 2 - l, 0, 0, tl / 2, 0, 0, tl / 2 - w / 2 / math.tan(r), -w / 2, 0, -tl / 2, -w / 2, 0)
-obj.drawpoly(tl / 2 - l, 0, 0, tl / 2, 0, 0, tl / 2 - w / 2 / math.tan(r), w / 2, 0, -tl / 2, w / 2, 0)
+obj.drawpoly(tl / 2 - length, 0, 0, tl / 2, 0, 0, tl / 2 - width / 2 / math.tan(r), -width / 2, 0, -tl / 2, -width / 2, 0)
+obj.drawpoly(tl / 2 - length, 0, 0, tl / 2, 0, 0, tl / 2 - width / 2 / math.tan(r), width / 2, 0, -tl / 2, width / 2, 0)
 
 obj.load("tempbuffer")
 
 --移動
-if track3 ~= 0 then
+if movement ~= 0 then
 	local rz = math.rad(obj.getvalue("rz"))
-	obj.ox = obj.ox + track3 * math.cos(rz)
-	obj.oy = obj.oy + track3 * math.sin(rz)
+	obj.ox = obj.ox + movement * math.cos(rz)
+	obj.oy = obj.oy + movement * math.sin(rz)
 end
