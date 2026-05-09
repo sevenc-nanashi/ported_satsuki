f1bafd5ddb4c4818e3901ff38710cb7e96f3eaa4
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-09 10:18:14 +0900

    feat: 円矢印を移植

--- scripts/ANM_ssd/円矢印.lua
+++ scripts/ANM_ssd/円矢印.lua
@@ -30,98 +30,54 @@ local jgh = 0
 ---$check:左右反転
 local syh = 0
 
----$value:中心角
+---$track:中心角
+---min=0
+---max=180
+---step=0.1
 local cc = 0
 
 ---$check:終点あり
-local siri = 0
+local siri = false
 
 --仮想バッファに描画
-kaso = math.max(size - haba + hds, size)
-obj.setoption("blend", "alpha_add")
-obj.setoption("drawtarget", "tempbuffer", kaso, kaso)
+local kaso = math.max(size - haba + hds, size)
+-- obj.setoption("blend", "alpha_add")
 
 --本体
 obj.load("figure", "円", col, size, haba)
 obj.effect("斜めクリッピング", "角度", 180 - r / 2, "ぼかし", 0)
 obj.effect("ミラー", "境目調整", -size / 2)
+obj.setoption("drawtarget", "tempbuffer", kaso, kaso)
 obj.draw(0, 0, 0, 1, 1, 0, 0, -90 + r / 2 * (1 - cc / 180))
 
 --頭
 obj.load("figure", fig, col, hds)
-l = size / 2 - haba / 2
-rr = math.rad(r - r * cc / 360)
+local l = size / 2 - haba / 2
+local rr = math.rad(r - r * cc / 360)
+local cl
 if fig == "三角形" then
-	cl = hds * math.sin(math.rad(30)) / 2 - 1
+  cl = hds * math.sin(math.rad(30)) / 2 - 1
 else
-	cl = 0
+  cl = 0
 end
-x = math.sin(rr) * l + math.cos(rr) * cl
-y = -math.cos(rr) * l + math.sin(rr) * cl
+local x = math.sin(rr) * l + math.cos(rr) * cl
+local y = -math.cos(rr) * l + math.sin(rr) * cl
 obj.draw(x, y, 0, 1, 1, 0, 0, 90 + r * (1 - cc / 360))
 
 --終点図形
-if siri == 1 then
-	rr2 = math.rad(-r * cc / 360)
-	if fig == "三角形" then
-		cl = hds * math.sin(math.rad(30)) / 2 - 1
-	else
-		cl = 0
-	end
-	x = math.sin(rr2) * l - math.cos(rr2) * cl
-	y = -math.cos(rr2) * l - math.sin(rr2) * cl
-	obj.draw(x, y, 0, 1, 1, 0, 0, -90 - r * cc / 360)
+if siri then
+  local rr2 = math.rad(-r * cc / 360)
+  if fig == "三角形" then
+    cl = hds * math.sin(math.rad(30)) / 2 - 1
+  else
+    cl = 0
+  end
+  x = math.sin(rr2) * l - math.cos(rr2) * cl
+  y = -math.cos(rr2) * l - math.sin(rr2) * cl
+  obj.draw(x, y, 0, 1, 1, 0, 0, -90 - r * cc / 360)
 end
 
 --仮想バッファから読み込み
 obj.load("tempbuffer")
 
 obj.effect("反転", "上下反転", jgh, "左右反転", syh)
-
---[[
-おまけ（講座動画バージョン）
-
---  --track0:サイズ,1,2000,100,1
---  --track1:ライン幅,1,1000,20,1
---  --track2:頭サイズ,0,1000,50,1
---  --track3:角度,0,360,260
---  ---$color:色
-local col = 0xffff00
-
----$figure:図形
-local fig = "三角形"
-
----$check:上下反転
-local jgh = 0
-
----$check:左右反転
-local syh = 0
-
----$value:中心角
-local cc = 0
-
----$check:終点あり
-local siri = 0
-
-
---仮想バッファに描画
-kaso=math.max(size-haba+hds,size)
-obj.setoption("drawtarget","tempbuffer",kaso,kaso)
-
-
---本体
-obj.load("figure","円",col,size,haba)
-obj.effect("斜めクリッピング","角度",180-r/2)
-obj.effect("ミラー","境目調整",-size/2)
-obj.draw(0,0,0,1,1,0,0,-90+r/2)
-
---頭
-obj.load("figure","三角形",col,hds)
-l = size/2 - haba/2
-x = math.sin(math.rad(r))*l
-y = -math.cos(math.rad(r))*l
-obj.draw(x,y,0,1,1,0,0,90+r)
-
---仮想バッファから読み込み
-obj.load("tempbuffer")
-]]
