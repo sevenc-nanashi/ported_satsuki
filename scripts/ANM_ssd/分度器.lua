b1f419cd6d095799d42b4d30efcd8bc79eb8d340
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-09 10:19:58 +0900

    feat: 分度器を移植

--- scripts/ANM_ssd/分度器.lua
+++ scripts/ANM_ssd/分度器.lua
@@ -3,12 +3,12 @@
 ---min=1
 ---max=256
 ---step=1
-local track0 = 34
+local size = 34
 ---$track:桁数
 ---min=0
 ---max=5
 ---step=1
-local track1 = 1
+local digits = 1
 ---$color:文字色
 local col1 = 0xffffff
 
@@ -16,16 +16,17 @@ local col1 = 0xffffff
 local col2 = 0x000000
 
 ---$value:座標
-local pos = {}
+local pos = { 0, -100, 0, 0, 100, 0 }
 
 obj.setanchor("pos", 3, "line")
-obj.setfont("MS UI Gothic", track0, 3, col1, col2)
+obj.setfont("MS UI Gothic", size, 3, col1, col2)
 local r1 = math.deg(math.atan2(pos[2] - pos[4], pos[1] - pos[3]) + math.pi)
 local r2 = math.deg(math.atan2(pos[6] - pos[4], pos[5] - pos[3]) + math.pi)
 local r0 = math.max(r1, r2) - math.min(r1, r2)
+local bdk
 if r0 > 180 then
-	bdk = 180 - r0 % 180
+  bdk = 180 - r0 % 180
 else
-	bdk = r0
+  bdk = r0
 end
-obj.load("text", math.floor(bdk * 10 ^ track1) / 10 ^ track1)
+obj.load("text", math.floor(bdk * 10 ^ digits) / 10 ^ digits)
