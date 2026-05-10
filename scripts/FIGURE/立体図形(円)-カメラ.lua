14570c6317414335296f3ed70e6017466f472e22
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-10 15:37:07 +0900

    feat: 立体図形（円）を移植

--- scripts/FIGURE/立体図形(円)-カメラ.lua
+++ scripts/FIGURE/立体図形(円)-カメラ.lua
@@ -3,45 +3,49 @@
 ---min=1
 ---max=1000
 ---step=1
-local s = 100
+local size = 100
 ---$track:奥行き
 ---min=1
 ---max=500
 ---step=1
-local w = 20
+local length = 20
 ---$track:ライン幅
 ---min=0
 ---max=4000
 ---step=1
-local track2 = 4000
+local weight = 4000
 ---$track:分割数
 ---min=3
 ---max=100
 ---step=1
-local n = 50
+local divisions = 50
 ---$color:色
 local color = 0xffffff
 
-h = math.min(track2, s / 2)
+local h = math.min(weight, size / 2)
 
-obj.load("figure", "円", color, s, h)
+obj.load("figure", "円", color, size, h)
 obj.effect()
-x = obj.w / 2
-y = obj.h / 2
-obj.drawpoly(-x, -y, w / 2, x, -y, w / 2, x, y, w / 2, -x, y, w / 2)
-obj.drawpoly(-x, -y, -w / 2, x, -y, -w / 2, x, y, -w / 2, -x, y, -w / 2)
+local x = obj.w / 2
+local y = obj.h / 2
+obj.drawpoly(-x, -y, length / 2, x, -y, length / 2, x, y, length / 2, -x, y, length / 2)
+obj.drawpoly(-x, -y, -length / 2, x, -y, -length / 2, x, y, -length / 2, -x, y, -length / 2)
 
 for k = 0, 1 do
-	s = s - 2 * h * k
-	obj.load("figure", "四角形", color, s / n)
-	obj.effect()
-	l = s / 2
-	r = 360 / n
-	for i = 0, n - 1 do
-		x0 = l * math.cos(math.pi / 180 * r * (i + 1))
-		x1 = l * math.cos(math.pi / 180 * r * i)
-		y0 = l * math.sin(math.pi / 180 * r * (i + 1))
-		y1 = l * math.sin(math.pi / 180 * r * i)
-		obj.drawpoly(x0, y0, -w / 2, x0, y0, w / 2, x1, y1, w / 2, x1, y1, -w / 2)
-	end
+  size = size - 2 * h * k
+  obj.load("figure", "四角形", color, math.ceil(size / divisions))
+  obj.effect()
+  local l = size / 2
+  local r = 360 / divisions
+  local polygons = {}
+  for i = 0, divisions - 1 do
+    local x0 = l * math.cos(math.pi / 180 * r * (i + 1))
+    local x1 = l * math.cos(math.pi / 180 * r * i)
+    local y0 = l * math.sin(math.pi / 180 * r * (i + 1))
+    local y1 = l * math.sin(math.pi / 180 * r * i)
+    table.insert(polygons,
+      { x0, y0, -length / 2, x0, y0, length / 2, x1, y1, length / 2, x1, y1, -length / 2, 0, 0, obj.w, 0, obj.w, obj.h, 0,
+        obj.h })
+  end
+  obj.drawpoly(polygons)
 end
