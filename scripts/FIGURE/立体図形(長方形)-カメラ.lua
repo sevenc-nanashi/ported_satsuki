1a4c9d4ae5a4930bfd0e03430d1d0bb867fec7d8
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-10 15:58:47 +0900

    feat: 立体図形（長方形）を移植

--- scripts/FIGURE/立体図形(長方形)-カメラ.lua
+++ scripts/FIGURE/立体図形(長方形)-カメラ.lua
@@ -3,17 +3,17 @@
 ---min=1
 ---max=1000
 ---step=1
-local s = 100
+local size = 100
 ---$track:奥行き
 ---min=1
 ---max=500
 ---step=1
-local w = 20
+local length = 20
 ---$track:ライン幅
 ---min=0
 ---max=4000
 ---step=1
-local track2 = 4000
+local weight = 4000
 ---$track:縦横比
 ---min=-100
 ---max=100
@@ -21,25 +21,25 @@ local aspect = 0
 ---$color:色
 local color = 0xffffff
 
-h = math.min(track2, s / 2)
-asx = math.max(aspect, 0)
-asy = math.max(-aspect, 0)
+local h = math.min(weight, size / 2)
+local asx = math.max(aspect, 0)
+local asy = math.max(-aspect, 0)
 
-obj.load("figure", "四角形", color, s, h)
+obj.load("figure", "四角形", color, size, h)
 obj.effect()
-x0 = s / 2 * (1 - asx / 100)
-y0 = s / 2 * (1 - asy / 100)
-obj.drawpoly(-x0, -y0, w / 2, x0, -y0, w / 2, x0, y0, w / 2, -x0, y0, w / 2)
-obj.drawpoly(-x0, -y0, -w / 2, x0, -y0, -w / 2, x0, y0, -w / 2, -x0, y0, -w / 2)
+local x0 = size / 2 * (1 - asx / 100)
+local y0 = size / 2 * (1 - asy / 100)
+obj.drawpoly(-x0, -y0, length / 2, x0, -y0, length / 2, x0, y0, length / 2, -x0, y0, length / 2)
+obj.drawpoly(-x0, -y0, -length / 2, x0, -y0, -length / 2, x0, y0, -length / 2, -x0, y0, -length / 2)
 
 for k = 0, 1 do
-	s = s - 2 * h * k
-	obj.load("figure", "四角形", color, s / 2)
+	size = size - 2 * h * k
+	obj.load("figure", "四角形", color, size / 2)
 	obj.effect()
-	x0 = s / 2 * (1 - asx / 100)
-	y0 = -s / 2 * (1 - asy / 100)
-	obj.drawpoly(x0, y0, -w / 2, x0, y0, w / 2, x0, -y0, w / 2, x0, -y0, -w / 2)
-	obj.drawpoly(-x0, y0, -w / 2, -x0, y0, w / 2, -x0, -y0, w / 2, -x0, -y0, -w / 2)
-	obj.drawpoly(-x0, y0, -w / 2, -x0, y0, w / 2, x0, y0, w / 2, x0, y0, -w / 2)
-	obj.drawpoly(-x0, -y0, -w / 2, -x0, -y0, w / 2, x0, -y0, w / 2, x0, -y0, -w / 2)
+	x0 = size / 2 * (1 - asx / 100)
+	y0 = -size / 2 * (1 - asy / 100)
+	obj.drawpoly(x0, y0, -length / 2, x0, y0, length / 2, x0, -y0, length / 2, x0, -y0, -length / 2)
+	obj.drawpoly(-x0, y0, -length / 2, -x0, y0, length / 2, -x0, -y0, length / 2, -x0, -y0, -length / 2)
+	obj.drawpoly(-x0, y0, -length / 2, -x0, y0, length / 2, x0, y0, length / 2, x0, y0, -length / 2)
+	obj.drawpoly(-x0, -y0, -length / 2, -x0, -y0, length / 2, x0, -y0, length / 2, x0, -y0, -length / 2)
 end
