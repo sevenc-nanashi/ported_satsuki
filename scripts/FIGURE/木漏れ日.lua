3b8d139b34bff42e444455013311c0428f9fd01b
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-10 10:51:03 +0900

    feat: 木漏れ日を移植

--- scripts/FIGURE/木漏れ日.lua
+++ scripts/FIGURE/木漏れ日.lua
@@ -2,10 +2,12 @@
 ---$track:ぼかし
 ---min=0
 ---max=200
-local track0 = 100
-obj.setoption("blend", 5)
-obj.load("figure", "背景", 0xffffff, 100)
+local blur = 100
+
+-- TODO: だいぶ重い...
+obj.setoption("blend", "overlay")
+obj.load("figure", "四角形", 0xffffff, math.max(obj.screen_h, obj.screen_w))
 obj.effect("ノイズ", "速度X", 0.9, "速度Y", 0.1, "周期X", 16, "周期Y", 0.01, "しきい値", 60.6)
-obj.effect("ぼかし", "範囲", track0, "縦横比", -100)
+obj.effect("ぼかし", "範囲", blur, "縦横比", -100, "サイズ固定", 1)
 obj.effect("極座標変換")
-obj.effect("発光", "強さ", 69.7, "拡散", 554, "しきい値", 44)
+obj.effect("発光", "強さ", 69.7, "拡散", 554, "しきい値", 44, "サイズ固定", 1)
