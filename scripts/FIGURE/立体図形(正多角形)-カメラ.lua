74e8f4c28a2da78d92b6932e4b00ad3deab2ff82
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-10 15:48:26 +0900

    feat: 立体図形（正多角形）を移植

--- scripts/FIGURE/立体図形(正多角形)-カメラ.lua
+++ scripts/FIGURE/立体図形(正多角形)-カメラ.lua
@@ -3,52 +3,60 @@
 ---min=1
 ---max=1000
 ---step=1
-local s = 100
+local size = 100
 ---$track:奥行き
 ---min=1
 ---max=500
 ---step=1
-local w = 20
+local length = 20
 ---$track:ライン幅
 ---min=0
 ---max=4000
 ---step=1
-local track2 = 4000
----$track:角数
----min=3
----max=6
----step=1
-local n = 4
+local weight = 4000
+---$select:図形
+---三角形=0
+---四角形=1
+---五角形=2
+---六角形=3
+local figure = 4
+
 ---$color:色
 local color = 0xffffff
 
-h = math.min(track2, s / 2)
-kaku = { "三角形", "四角形", "五角形", "六角形" }
+local h = math.min(weight, size / 2)
 
-obj.load("figure", kaku[n - 2], color, s, h)
+local figure_names = { "三角形", "四角形", "五角形", "六角形" }
+obj.load("figure", figure_names[figure + 1], color, size, h)
 obj.effect()
-x = obj.w / 2
-y = obj.h / 2
-obj.drawpoly(-x, -y, w / 2, x, -y, w / 2, x, y, w / 2, -x, y, w / 2)
-obj.drawpoly(-x, -y, -w / 2, x, -y, -w / 2, x, y, -w / 2, -x, y, -w / 2)
+local x = obj.w / 2
+local y = obj.h / 2
+obj.drawpoly(-x, -y, length / 2, x, -y, length / 2, x, y, length / 2, -x, y, length / 2)
+obj.drawpoly(-x, -y, -length / 2, x, -y, -length / 2, x, y, -length / 2, -x, y, -length / 2)
 
+local corners = figure + 3
 for k = 0, 1 do
-	s = s - 2 * h * k
-	if n == 4 then
-		l = s / 2 * math.sqrt(2)
-		sr = 45
-	else
-		l = s / 2
-		sr = 90
-	end
-	obj.load("figure", "四角形", color, 2)
-	obj.effect()
-	r = 360 / n
-	for i = 0, n - 1 do
-		x0 = l * math.cos((sr + r * i) * math.pi / 180)
-		x1 = l * math.cos((sr - r + r * i) * math.pi / 180)
-		y0 = -l * math.sin((sr + r * i) * math.pi / 180)
-		y1 = -l * math.sin((sr - r + r * i) * math.pi / 180)
-		obj.drawpoly(x0, y0, -w / 2, x0, y0, w / 2, x1, y1, w / 2, x1, y1, -w / 2)
-	end
+  size = size - 2 * h * k
+  local l, sr
+  if corners == 4 then
+    l = size / 2 * math.sqrt(2) - 1
+    sr = 45
+  else
+    l = size / 2 - 1
+    sr = 90
+  end
+  obj.load("figure", "四角形", color, 2)
+  obj.effect()
+  local r = 360 / corners
+  local polygons = {}
+  for i = 0, corners - 1 do
+    local x0 = l * math.cos((sr + r * i) * math.pi / 180)
+    local x1 = l * math.cos((sr - r + r * i) * math.pi / 180)
+    local y0 = -l * math.sin((sr + r * i) * math.pi / 180)
+    local y1 = -l * math.sin((sr - r + r * i) * math.pi / 180)
+    table.insert(polygons,
+      { x0, y0, -length / 2, x0, y0, length / 2, x1, y1, length / 2, x1, y1, -length / 2, 0, 0, obj.w, 0, obj.w, obj.h,
+        0, obj.h })
+  end
+  obj.drawpoly(polygons)
 end
