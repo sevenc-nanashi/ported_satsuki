1f728125a4a2fee6794f66078962edb7d0a45c41
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-10 10:52:13 +0900

    feat: 矢印を移植

--- scripts/FIGURE/矢印.lua
+++ scripts/FIGURE/矢印.lua
@@ -2,33 +2,33 @@
 ---$track:長さ
 ---min=0
 ---max=5000
-local l = 100
+local length = 100
 ---$track:幅
 ---min=0
 ---max=500
-local w = 20
+local weight = 20
 ---$track:3サイズ
 ---min=0
 ---max=1000
-local s = 50
+local tip_size = 50
 ---$track:3横幅
 ---min=0
 ---max=1000
-local track3 = 100
+local tip_width = 100
 ---$color:色
 local color = 0xffffff
 
-tw = track3 / 100
-b = 2 --位置調整
+local tw = tip_width / 100
+local b = 2 --位置調整
 
-obj.load("figure", "三角形", color, s * 2)
-sx = s / 2 * tw
-sy = -l - s / 2 - s / 2 * math.sin(math.pi * 30 / 180) + b
-obj.setoption("dst", "tmp", (l + s) * 2, (l + s) * 2)
-obj.drawpoly(-sx, sy, 0, sx, sy, 0, sx, sy + s, 0, -sx, sy + s, 0)
+obj.load("figure", "三角形", color, tip_size * 2)
+local sx = tip_size / 2 * tw
+local sy = -length - tip_size / 2 - tip_size / 2 * math.sin(math.pi * 30 / 180) + b
+obj.setoption("drawtarget", "tempbuffer", (length + tip_size) * 2, (length + tip_size) * 2)
+obj.drawpoly(-sx, sy, 0, sx, sy, 0, sx, sy + tip_size, 0, -sx, sy + tip_size, 0)
 
 obj.load("figure", "四角形", color, 2)
-obj.setoption("dst", "tmp")
-obj.drawpoly(-w / 2, -l, 0, w / 2, -l, 0, w / 2, 0, 0, -w / 2, 0, 0)
+obj.setoption("drawtarget", "tempbuffer")
+obj.drawpoly(-weight / 2, -length, 0, weight / 2, -length, 0, weight / 2, 0, 0, -weight / 2, 0, 0)
 
 obj.load("tempbuffer")
