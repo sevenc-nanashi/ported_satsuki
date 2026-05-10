f326f0da505eceb0faf1bf88f93662cff6f1bf23
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-10 09:29:03 +0900

    feat: 左寄せを移植

--- scripts/削除済み/左寄せとか.lua
+++ scripts/削除済み/左寄せとか.lua
@@ -2,10 +2,10 @@
 ---$track:X軸[%]
 ---min=-100
 ---max=100
-local track0 = 0
+local x_shift = 0
 ---$track:Y軸[%]
 ---min=-100
 ---max=100
-local track1 = 0
-obj.ox = obj.w / 2 * track0 / 100
-obj.oy = obj.h / 2 * track1 / 100
+local y_shift = 0
+obj.ox = obj.w / 2 * x_shift / 100
+obj.oy = obj.h / 2 * y_shift / 100
