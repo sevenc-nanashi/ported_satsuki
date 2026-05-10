41438c4a168aff8e0e82cfda8242cc14b3b4371f
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-10 10:19:10 +0900

    feat: 星型図形Plusを移植

--- scripts/FIGURE/星型図形Plus.lua
+++ scripts/FIGURE/星型図形Plus.lua
@@ -2,39 +2,39 @@
 ---$track:半径
 ---min=1
 ---max=2000
-local l = 100
+local radius = 100
 ---$track:内角
 ---min=1
 ---max=108
-local track1 = 36
+local inner_angle = 36
 ---$track:ライン幅
 ---min=0
 ---max=100
-local track2 = 0
+local weight = 0
 ---$track:透明度
 ---min=0
 ---max=100
-local track3 = 0
+local alpha = 0
 ---$color:色
 local color = 0xffffff
 
-r = math.rad(track1)
+local r = math.rad(inner_angle)
 
 obj.load("figure", "四角形", color, 2)
-x = math.tan(r / 2) * l
-obj.setoption("dst", "tmp", l * 2, l * 2)
-obj.drawpoly(-x, 0, 0, -x, 0, 0, 0, -l, 0, x, 0, 0)
+local x = math.tan(r / 2) * radius
+obj.setoption("drawtarget", "tempbuffer", radius * 2, radius * 2)
+obj.drawpoly(-x, 0, 0, -x, 0, 0, 0, -radius, 0, x, 0, 0)
 
 obj.load("tempbuffer")
-obj.effect("斜めクリッピング", "中心Y", 5, "角度", track1 * 54 / 108)
-obj.effect("斜めクリッピング", "中心Y", 5, "角度", -track1 * 54 / 108)
-obj.setoption("dst", "tmp", l * 2, l * 2)
+obj.effect("斜めクリッピング", "中心Y", 5, "角度", inner_angle * 54 / 108)
+obj.effect("斜めクリッピング", "中心Y", 5, "角度", -inner_angle * 54 / 108)
+obj.setoption("dst", "tmp", radius * 2, radius * 2)
 for i = 0, 4 do
 	obj.draw(0, 0, 0, 1, 1, 0, 0, 72 * i)
 end
 
 obj.load("tempbuffer")
-obj.setoption("dst", "tmp")
+obj.setoption("drawtarget", "tempbuffer")
 obj.setoption("blend", "alpha_sub")
-obj.draw(0, 0, 0, track2 / 100, 1 - track3 / 100)
+obj.draw(0, 0, 0, weight / 100, 1 - alpha / 100)
 obj.load("tempbuffer")
