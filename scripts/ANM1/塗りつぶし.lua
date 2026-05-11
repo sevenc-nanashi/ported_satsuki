2ad815e488cd7fdd15a1b30b336b1b94028ec5b6
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-11 13:41:31 +0900

    feat: 塗りつぶしを移植

--- scripts/ANM1/塗りつぶし.lua
+++ scripts/ANM1/塗りつぶし.lua
@@ -2,18 +2,28 @@
 ---$track:モノクロ
 ---min=0
 ---max=100
-local track0 = 0
+local monochrome = 0
 ---$track:強さ
 ---min=0
 ---max=100
-local track1 = 100
----$track:合成
----min=0
----max=9
----step=1
-local track2 = 0
+local strength = 100
+---$select:合成
+---通常=0
+---加算=1
+---減算=2
+---乗算=3
+---スクリーン=4
+---オーバーレイ=5
+---比較(明)=6
+---比較(明)=7
+---輝度=8
+---色差=9
+---陰影=10
+---明暗=11
+---差分=12
+local blending = 0
 ---$color:色
 local color = 0xffffff
 
-obj.effect("単色化", "強さ", track0)
-obj.effect("グラデーション", "強さ", track1, "color", color, "color2", color, "blend", track2)
+obj.effect("単色化", "強さ", monochrome)
+obj.effect("グラデーション", "強さ", strength, "color", color, "color2", color, "blend", blending)
