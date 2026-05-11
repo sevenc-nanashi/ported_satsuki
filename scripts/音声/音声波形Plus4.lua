9abcf8a02f28d05055087cdb8d634e5c5ef68aed
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-11 13:13:50 +0900

    feat: 音声波形Plus4を移植

--- scripts/音声/音声波形Plus4.lua
+++ scripts/音声/音声波形Plus4.lua
@@ -3,22 +3,25 @@
 ---min=2
 ---max=1024
 ---step=1
-local n = 51
+local n_divisions = 51
 ---$track:ライン幅
 ---min=1
 ---max=100
-local track1 = 3
+local weight = 3
 ---$track:横幅
 ---min=1
 ---max=2000
-local W = 640
+local width = 640
 ---$track:閾値
 ---min=1
 ---max=2000
 ---step=1
-local track3 = 500
----$value:高さ[%]
-local H = 150
+local threshold = 500
+---$track:高さ[%]
+---min=0
+---max=800
+---step=0.1
+local height = 150
 
 ---$color:色
 local color = 0x00ff00
@@ -29,27 +32,28 @@ local kaku = 1
 ---$check:閾値を減算
 local sist = 1
 
-l = track1 / 2
-siki = track3 * 10
-w = W / (n - 1)
-bun = math.floor(1024 / n)
-buf = {}
+local l = weight / 2
+local siki = threshold * 10
+local w = width / (n_divisions - 1)
+local bun = math.floor(1024 / n_divisions)
+local buf = {}
 obj.getaudio(buf, "audiobuffer", "pcm", 1024)
-obj.setoption("dst", "tmp", W, W)
+obj.setoption("dst", "tmp", width, width)
 
 --ラインの描画
 -- obj.setoption("blend", "alpha_mix")
 obj.load("figure", "四角形", color, 1)
-y0 = 0
-y1 = 0
-for i = 1, n - 1 do
-    x0 = (i - 1) * w - W / 2
-    x1 = i * w - W / 2
+local y0 = 0
+local y1 = 0
+local polygons = {}
+for i = 1, n_divisions - 1 do
+    local x0 = (i - 1) * w - width / 2
+    local x1 = i * w - width / 2
     if math.abs(buf[bun * (i + 1)]) > siki then
         if buf[bun * (i + 1)] > 0 then
-            y1 = -(buf[bun * (i + 1)] - siki * sist) / 200 * H / 100
+            y1 = -(buf[bun * (i + 1)] - siki * sist) / 200 * height / 100
         else
-            y1 = -(buf[bun * (i + 1)] + siki * sist) / 200 * H / 100
+            y1 = -(buf[bun * (i + 1)] + siki * sist) / 200 * height / 100
         end
     end
     if kaku == 1 then
@@ -59,29 +63,33 @@ for i = 1, n - 1 do
             y1 = 0
         end
     end
-    r = math.atan2(y0 - y1, x1 - x0)
-    dx = math.sin(r) * l
-    dy = math.cos(r) * l
-    obj.drawpoly(x0 - dx, y0 - dy, 0, x1 - dx, y1 - dy, 0, x1 + dx, y1 + dy, 0, x0 + dx, y0 + dy, 0)
+    local r = math.atan2(y0 - y1, x1 - x0)
+    local dx = math.sin(r) * l
+    local dy = math.cos(r) * l
+    table.insert(polygons, { x0 - dx, y0 - dy, 0, x1 - dx, y1 - dy, 0, x1 + dx, y1 + dy, 0, x0 + dx, y0 + dy, 0,
+        0, 0, obj.w, 0, obj.w, obj.h, 0, obj.h })
     y0 = y1
 end
+obj.drawpoly(polygons)
 
+local polygons2 = {}
 --頂点の描画
 obj.load("figure", "円", color, l * 8)
-for i = 1, n - 1 do
-    x0 = (i - 1) * w - W / 2
-    y0 = 0
+for i = 1, n_divisions - 1 do
+    local x0 = (i - 1) * w - width / 2
+    local y0 = 0
     if math.abs(buf[bun * i]) > siki then
         if buf[bun * i] > 0 then
-            y0 = -(buf[bun * i] - siki * sist) / 200 * H / 100
+            y0 = -(buf[bun * i] - siki * sist) / 200 * height / 100
         else
-            y0 = -(buf[bun * i] + siki * sist) / 200 * H / 100
+            y0 = -(buf[bun * i] + siki * sist) / 200 * height / 100
         end
     end
     if kaku == 1 and i % 2 == 1 then
         y0 = 0
     end
-    obj.drawpoly(x0 - l, y0 - l, 0, x0 + l, y0 - l, 0, x0 + l, y0 + l, 0, x0 - l, y0 + l, 0)
+    table.insert(polygons2, { x0 - l, y0 - l, 0, x0 + l, y0 - l, 0, x0 + l, y0 + l, 0, x0 - l, y0 + l, 0, 0, 0, obj.w, 0, obj.w, obj.h, 0, obj.h })
 end
+obj.drawpoly(polygons2)
 
 obj.load("tempbuffer")
