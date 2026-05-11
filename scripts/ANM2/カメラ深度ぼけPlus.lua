a622bb3d37e56976517ed6f37f5976bf98202764
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-12 08:38:17 +0900

    feat: カメラ深度ぼけPlusを移植

--- scripts/ANM2/カメラ深度ぼけPlus.lua
+++ scripts/ANM2/カメラ深度ぼけPlus.lua
@@ -3,26 +3,27 @@
 ---min=0
 ---max=1000
 ---step=1
-local track0 = 50
+local range = 50
 ---$track:Z基準
 ---min=-20000
 ---max=20000
-local track1 = 0
+local base_z = 0
 ---$track:最大値
 ---min=0
 ---max=1000
 ---step=1
-local track2 = 300
+local max = 300
 ---$check:奥をぼかす
 local oku = 1
 
 ---$check:手前をぼかす
 local mae = 1
 
-if obj.z > track1 then
-    l = (obj.z - track1) / 10000 * oku
+local l
+if obj.z > base_z then
+    l = (obj.z - base_z) / 10000 * oku
 else
-    l = (track1 - obj.z) / 10000 * mae
+    l = (base_z - obj.z) / 10000 * mae
 end
-l = math.min(l * track0, track2)
+l = math.min(l * range, max)
 obj.effect("ぼかし", "範囲", l)
