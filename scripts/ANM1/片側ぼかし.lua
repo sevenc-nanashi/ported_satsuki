07a9be26df159dad5c6273c5cdfde327d66cb6f0
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-12 09:38:53 +0900

    feat: 片側ぼかしを移植

--- scripts/ANM1/片側ぼかし.lua
+++ scripts/ANM1/片側ぼかし.lua
@@ -2,20 +2,20 @@
 ---$track:上
 ---min=0
 ---max=2000
-local track0 = 0
+local up_blur = 0
 ---$track:下
 ---min=0
 ---max=2000
-local track1 = 0
+local down_blur = 0
 ---$track:左
 ---min=0
 ---max=2000
-local track2 = 0
+local left_blur = 0
 ---$track:右
 ---min=0
 ---max=2000
-local track3 = 0
-obj.effect("斜めクリッピング", "中心Y", -obj.h / 2 + track0 / 2, "角度", 180, "ぼかし", track0) --上
-obj.effect("斜めクリッピング", "中心Y", obj.h / 2 - track1 / 2, "角度", 0, "ぼかし", track1) --下
-obj.effect("斜めクリッピング", "中心X", -obj.w / 2 + track2 / 2, "角度", 90, "ぼかし", track2) --左
-obj.effect("斜めクリッピング", "中心X", obj.w / 2 - track3 / 2, "角度", 270, "ぼかし", track3) --右
+local right_blur = 0
+obj.effect("斜めクリッピング", "中心Y", -obj.h / 2 + up_blur / 2, "角度", 180, "ぼかし", up_blur) --上
+obj.effect("斜めクリッピング", "中心Y", obj.h / 2 - down_blur / 2, "角度", 0, "ぼかし", down_blur) --下
+obj.effect("斜めクリッピング", "中心X", -obj.w / 2 + left_blur / 2, "角度", 90, "ぼかし", left_blur) --左
+obj.effect("斜めクリッピング", "中心X", obj.w / 2 - right_blur / 2, "角度", 270, "ぼかし", right_blur) --右
