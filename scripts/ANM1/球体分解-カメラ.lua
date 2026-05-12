52e2af0fae8a0bc00cce4a31c1f2d33ef9a5797e
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-12 15:12:06 +0900

    feat: 球体分解を移植

--- scripts/ANM1/球体分解-カメラ.lua
+++ scripts/ANM1/球体分解-カメラ.lua
@@ -6,46 +6,50 @@ local k = 10
 ---$track:中心X
 ---min=-100
 ---max=100
-local track1 = 0
+local center_x = 0
 ---$track:中心Y
 ---min=-100
 ---max=100
-local track2 = 0
+local center_y = 0
+--trackgroup@center_x,center_y:中心
 ---$check:反転
-local __rename_me_check0 = false
+local invert = false
 
 --obj.setoption('antialias',0)
 obj.effect("反転", "上下反転", "1")
 obj.effect()
-if __rename_me_check0 then
+if invert then
     obj.effect("反転", "左右反転", "1")
     obj.ry = 180
 end
-w = 28
-h = 28
-wd = w * k / 10
-hd = h * k / 10
-cx = math.floor(w / 2 * track1 / 100)
-cy = math.floor(h / 2 * track2 / 100)
-r = obj.w / w / math.sin(math.pi * 2 / wd)
+local w = 28
+local h = 28
+local wd = w * k / 10
+local hd = h * k / 10
+local cx = math.floor(w / 2 * center_x / 100)
+local cy = math.floor(h / 2 * center_y / 100)
+local r = obj.w / w / math.sin(math.pi * 2 / wd)
+local polygons = {}
 for y = -h / 2 + cy, h / 2 + cy - 1 do
-    y0 = -r * math.sin(math.pi * y / hd)
-    r0 = r * math.cos(math.pi * y / hd)
-    y1 = -r * math.sin(math.pi * (y + 1) / hd)
-    r1 = r * math.cos(math.pi * (y + 1) / hd)
-    v0 = obj.h * (y + h / 2 - cy) / h
-    v1 = obj.h * (y + h / 2 - cy + 1) / h
+    local y0 = -r * math.sin(math.pi * y / hd)
+    local r0 = r * math.cos(math.pi * y / hd)
+    local y1 = -r * math.sin(math.pi * (y + 1) / hd)
+    local r1 = r * math.cos(math.pi * (y + 1) / hd)
+    local v0 = obj.h * (y + h / 2 - cy) / h
+    local v1 = obj.h * (y + h / 2 - cy + 1) / h
     for x = -w / 2 + cx, w / 2 + cx - 1 do
-        x0 = r0 * math.sin(math.pi * 2 * x / wd)
-        x1 = r0 * math.sin(math.pi * 2 * (x + 1) / wd)
-        x2 = r1 * math.sin(math.pi * 2 * (x + 1) / wd)
-        x3 = r1 * math.sin(math.pi * 2 * x / wd)
-        z0 = -r0 * math.cos(math.pi * 2 * x / wd) + r
-        z1 = -r0 * math.cos(math.pi * 2 * (x + 1) / wd) + r
-        z2 = -r1 * math.cos(math.pi * 2 * (x + 1) / wd) + r
-        z3 = -r1 * math.cos(math.pi * 2 * x / wd) + r
-        u0 = obj.w * (x + w / 2 - cx) / w
-        u1 = obj.w * (x + w / 2 - cx + 1) / w
-        obj.drawpoly(x0, y0, z0, x1, y0, z1, x2, y1, z2, x3, y1, z3, u0, v0, u1, v0, u1, v1, u0, v1)
+        local x0 = r0 * math.sin(math.pi * 2 * x / wd)
+        local x1 = r0 * math.sin(math.pi * 2 * (x + 1) / wd)
+        local x2 = r1 * math.sin(math.pi * 2 * (x + 1) / wd)
+        local x3 = r1 * math.sin(math.pi * 2 * x / wd)
+        local z0 = -r0 * math.cos(math.pi * 2 * x / wd) + r
+        local z1 = -r0 * math.cos(math.pi * 2 * (x + 1) / wd) + r
+        local z2 = -r1 * math.cos(math.pi * 2 * (x + 1) / wd) + r
+        local z3 = -r1 * math.cos(math.pi * 2 * x / wd) + r
+        local u0 = obj.w * (x + w / 2 - cx) / w
+        local u1 = obj.w * (x + w / 2 - cx + 1) / w
+
+        table.insert(polygons, {x0, y0, z0, x1, y0, z1, x2, y1, z2, x3, y1, z3, u0, v0, u1, v0, u1, v1, u0, v1})
     end
 end
+obj.drawpoly(polygons)
