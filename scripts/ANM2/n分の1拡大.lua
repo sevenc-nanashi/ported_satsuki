786ef64ce97ef7441a88fda3a9be967a4d9f31fe
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-12 11:47:44 +0900

    feat: n分の1拡大を移植

--- scripts/ANM2/n分の1拡大.lua
+++ scripts/ANM2/n分の1拡大.lua
@@ -2,5 +2,7 @@
 ---$track:100/n
 ---min=1
 ---max=2000
-local track0 = 100
-obj.zoom = obj.zoom * 100 / track0
+local inverse = 100
+obj.sx = obj.sx * 100 / inverse
+obj.sy = obj.sy * 100 / inverse
+obj.sz = obj.sz * 100 / inverse
