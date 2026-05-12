f6054398e7af2d50721ad8aac5f5417b44eb42b1
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-12 13:56:04 +0900

    feat: ロールを移植

--- scripts/ANM1/ロール-カメラ.lua
+++ scripts/ANM1/ロール-カメラ.lua
@@ -16,48 +16,53 @@ local hanten = 0
 ---$check:軸を中心に
 local m = 0
 
----$value:分割数
+---$track:分割数
+---min=1
+---max=100
+---step=1
 local n = 28
 
 obj.effect()
-nd = n * track0 / 10
-c = math.floor(n / 2 * track1 / 100)
+local nd = n * track0 / 10
+local c = math.floor(n / 2 * track1 / 100)
+local polygons = {}
 if tate == 0 then
-    r = obj.w / n / math.sin(math.pi * 2 / nd)
+    local r = obj.w / n / math.sin(math.pi * 2 / nd)
     if hanten == 1 then
         obj.effect("反転", "左右反転", "1")
         obj.ry = 180
     end
-    y0 = -obj.h / 2
-    y1 = obj.h / 2
-    v0 = 0
-    v1 = obj.h
+    local y0 = -obj.h / 2
+    local y1 = obj.h / 2
+    local v0 = 0
+    local v1 = obj.h
     for x = -n / 2 + c, n / 2 + c - 1 do
-        x0 = r * math.sin(math.pi * 2 * x / nd)
-        x1 = r * math.sin(math.pi * 2 * (x + 1) / nd)
-        z0 = -r * math.cos(math.pi * 2 * x / nd) + r * (1 - m)
-        z1 = -r * math.cos(math.pi * 2 * (x + 1) / nd) + r * (1 - m)
-        u0 = obj.w * (x + n / 2 - c) / n
-        u1 = obj.w * (x + n / 2 + 1 - c) / n
-        obj.drawpoly(x0, y0, z0, x1, y0, z1, x1, y1, z1, x0, y1, z0, u0, v0, u1, v0, u1, v1, u0, v1)
+        local x0 = r * math.sin(math.pi * 2 * x / nd)
+        local x1 = r * math.sin(math.pi * 2 * (x + 1) / nd)
+        local z0 = -r * math.cos(math.pi * 2 * x / nd) + r * (1 - m)
+        local z1 = -r * math.cos(math.pi * 2 * (x + 1) / nd) + r * (1 - m)
+        local u0 = obj.w * (x + n / 2 - c) / n
+        local u1 = obj.w * (x + n / 2 + 1 - c) / n
+        table.insert(polygons, {x0, y0, z0, x1, y0, z1, x1, y1, z1, x0, y1, z0, u0, v0, u1, v0, u1, v1, u0, v1})
     end
 else
-    r = obj.h / n / math.sin(math.pi * 2 / nd)
+    local r = obj.h / n / math.sin(math.pi * 2 / nd)
     if hanten == 1 then
         obj.effect("反転", "上下反転", "1")
         obj.rx = 180
     end
-    x0 = -obj.w / 2
-    x1 = obj.w / 2
-    u0 = 0
-    u1 = obj.w
+    local x0 = -obj.w / 2
+    local x1 = obj.w / 2
+    local u0 = 0
+    local u1 = obj.w
     for y = -n / 2 + c, n / 2 + c - 1 do
-        y0 = r * math.cos(math.pi * (2 * y / nd - 0.5))
-        y1 = r * math.cos(math.pi * (2 * (y + 1) / nd - 0.5))
-        z0 = r * math.sin(math.pi * (2 * y / nd - 0.5)) + r * (1 - m)
-        z1 = r * math.sin(math.pi * (2 * (y + 1) / nd - 0.5)) + r * (1 - m)
-        v0 = obj.h * (y + n / 2 - c) / n
-        v1 = obj.h * (y + n / 2 - c + 1) / n
-        obj.drawpoly(x0, y0, z0, x1, y0, z0, x1, y1, z1, x0, y1, z1, u0, v0, u1, v0, u1, v1, u0, v1)
+        local y0 = r * math.cos(math.pi * (2 * y / nd - 0.5))
+        local y1 = r * math.cos(math.pi * (2 * (y + 1) / nd - 0.5))
+        local z0 = r * math.sin(math.pi * (2 * y / nd - 0.5)) + r * (1 - m)
+        local z1 = r * math.sin(math.pi * (2 * (y + 1) / nd - 0.5)) + r * (1 - m)
+        local v0 = obj.h * (y + n / 2 - c) / n
+        local v1 = obj.h * (y + n / 2 - c + 1) / n
+        table.insert(polygons, {x0, y0, z0, x1, y0, z0, x1, y1, z1, x0, y1, z1, u0, v0, u1, v0, u1, v1, u0, v1})
     end
 end
+obj.drawpoly(polygons)
