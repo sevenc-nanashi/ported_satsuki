662e862b68c0705f987bbe1bc71ad259bd2506a2
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-12 17:28:43 +0900

    feat: 円形並べ-カメラを移植

--- scripts/ANM1/円形並べ-カメラ.lua
+++ scripts/ANM1/円形並べ-カメラ.lua
@@ -1,34 +1,35 @@
 --label:${ROOT_CATEGORY}\配置
 ---$track:個数
----min=0
+---min=1
 ---max=100
 ---step=1
-local n = 10
+local count = 10
 ---$track:距離
 ---min=0
 ---max=2000
-local l = 200
+---step=1
+local distance = 200
 ---$track:拡大率
 ---min=0
 ---max=800
-local track2 = 100
+local scale = 100
 ---$track:回転
 ---min=-360
 ---max=360
-local rr = 0
+local rotation = 0
 ---$check:Y軸回転なし
-local __rename_me_check0 = false
+local no_y_rotation = false
 
-k = track2 / 100
+local scale_rate = scale / 100
 obj.effect()
-for i = 0, n - 1 do
-    r = 360 * i / n + rr
-    x = math.sin(r * math.pi / 180) * l * k
-    z = -math.cos(r * math.pi / 180) * l * k
-    s = obj.zoom * k
-    ry = -360 * i / n - rr
-    if __rename_me_check0 then
-        ry = ry * 0
+for i = 0, count - 1 do
+    local angle = 360 * i / count + rotation
+    local x = math.sin(angle * math.pi / 180) * distance * scale_rate
+    local z = -math.cos(angle * math.pi / 180) * distance * scale_rate
+    local zoom = obj.zoom * scale_rate
+    local y_rotation = -angle
+    if no_y_rotation then
+        y_rotation = 0
     end
-    obj.draw(x, 0, z, s, 1, 0, ry, 0)
+    obj.draw(x, 0, z, zoom, 1, 0, y_rotation, 0)
 end
