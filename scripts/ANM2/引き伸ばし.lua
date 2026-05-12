46fc5ecc68f9df17ed7dbd5f52c4e7f22f0b1192
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-12 14:10:33 +0900

    feat: 引き伸ばしを移植

--- scripts/ANM2/引き伸ばし.lua
+++ scripts/ANM2/引き伸ばし.lua
@@ -2,19 +2,19 @@
 ---$track:伸度
 ---min=0
 ---max=800
-local track0 = 0
+local length = 0
 ---$track:幅[%]
 ---min=0.1
 ---max=100
-local track1 = 10
+local width = 10
 ---$track:基準[%]
 ---min=-100
 ---max=100
-local track2 = 0
+local basis = 0
 ---$track:中心[%]
 ---min=-100
 ---max=100
-local track3 = 0
+local center = 0
 ---$check:縦方向
 local dir = 0
 
@@ -22,35 +22,35 @@ local dir = 0
 local alp = 0
 
 obj.effect()
-w = obj.w / 2
-h = obj.h / 2
+local w = obj.w / 2
+local h = obj.h / 2
 if dir < 1 then
-    natto = obj.w * track0 / 100
-    haba = obj.w * track1 / 100
-    c = (obj.w - haba) / 2 * track2 / 100
-    t = natto * track3 / 100
+    local natto = obj.w * length / 100
+    local haba = obj.w * width / 100
+    local c = (obj.w - haba) / 2 * basis / 100
+    local t = natto * center / 100
 
-    c0 = c - haba / 2 - natto + t
-    c1 = c + haba / 2 + natto + t
-    x0 = -w - natto + t
-    x1 = w + natto + t
-    u0 = w + c - haba / 2
-    u1 = w + c + haba / 2
+    local c0 = c - haba / 2 - natto + t
+    local c1 = c + haba / 2 + natto + t
+    local x0 = -w - natto + t
+    local x1 = w + natto + t
+    local u0 = w + c - haba / 2
+    local u1 = w + c + haba / 2
     obj.drawpoly(x0, -h, 0, c0 + 1, -h, 0, c0 + 1, h, 0, x0, h, 0, 0, 0, u0 + 1, 0, u0 + 1, h * 2, 0, h * 2)
     obj.drawpoly(c1 - 1, -h, 0, x1, -h, 0, x1, h, 0, c1 - 1, h, 0, u1 - 1, 0, w * 2, 0, w * 2, h * 2, u1 - 1, h * 2)
     obj.drawpoly(c0, -h, 0, c1, -h, 0, c1, h, 0, c0, h, 0, u0, 0, u1, 0, u1, h * 2, u0, h * 2, 1 - alp / 2)
 else
-    natto = obj.h * track0 / 100
-    haba = obj.h * track1 / 100
-    c = (obj.h - haba) / 2 * track2 / 100
-    t = natto * track3 / 100
+    local natto = obj.h * length / 100
+    local haba = obj.h * width / 100
+    local c = (obj.h - haba) / 2 * basis / 100
+    local t = natto * center / 100
 
-    c0 = c - haba / 2 - natto + t
-    c1 = c + haba / 2 + natto + t
-    y0 = -h - natto + t
-    y1 = h + natto + t
-    v0 = h + c - haba / 2
-    v1 = h + c + haba / 2
+    local c0 = c - haba / 2 - natto + t
+    local c1 = c + haba / 2 + natto + t
+    local y0 = -h - natto + t
+    local y1 = h + natto + t
+    local v0 = h + c - haba / 2
+    local v1 = h + c + haba / 2
     obj.drawpoly(-w, y0, 0, w, y0, 0, w, c0 + 1, 0, -w, c0 + 1, 0, 0, 0, w * 2, 0, w * 2, v0 + 1, 0, v0 + 1)
     obj.drawpoly(-w, c1 - 1, 0, w, c1 - 1, 0, w, y1, 0, -w, y1, 0, 0, v1 - 1, w * 2, v1 - 1, w * 2, h * 2, 0, h * 2)
     obj.drawpoly(-w, c0, 0, w, c0, 0, w, c1, 0, -w, c1, 0, 0, v0, w * 2, v0, w * 2, v1, 0, v1, 1 - alp / 2)
