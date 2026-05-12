0682819ffb3f3de72aa8a847443520140f516076
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-12 10:42:59 +0900

    feat: キラキラを移植

--- scripts/ANM1/キラキラ.lua
+++ scripts/ANM1/キラキラ.lua
@@ -13,54 +13,75 @@ local l = 300
 ---min=1
 ---max=2000
 ---step=1
-local track2 = 100
+local speed = 100
 ---$track:出現範囲
 ---min=0
 ---max=2000
 local sl = 0
----$value:ランダム速度[-%]
+---$track:ランダム速度[-%]
+---min=0
+---max=100
+---step=1
 local v_ran = 90
 
----$value:透過速度[%]
+---$track:透過速度[%]
+---min=0
+---max=400
+---step=1
 local av = 100
 
----$value:拡大速度[%]
+---$track:拡大速度[%]
+---min=-400
+---max=400
+---step=1
 local sv = 0
 
 ---$check:ランダム回転
 local rz_ran = 0
 
----$value:回転速度[度]
+---$track:回転速度[度]
+---min=-3600
+---max=3600
+---step=1
 local rzv_ran = 0
 
----$value:ランダム拡大[-%]
+---$track:ランダム拡大[-%]
+---min=0
+---max=100
+---step=1
 local s_ran = 0
-
----$value:減速度[1-5]
-local beki = 3
 
 ---$check:終点で消える
 local zero = 1
 
----$value:乱数
+---$track:減速度
+---min=1
+---max=5
+---step=1
+local beki = 3
+
+---$track:乱数
+---min=0
+---max=100
+---step=1
 local ran = 0
 
 obj.effect()
-v = l / track2 --距離を進むのにかかる時間
+local v = l / speed --距離を進むのにかかる時間
 for i = 0, n - 1 do
-    t = math.max((v - obj.time) / v, 0)
+    local t = math.max((v - obj.time) / v, 0)
     t = t ^ beki
-    x = (l * (1 - t) + obj.rand(0, sl, i, ran))
+    local x = (l * (1 - t) + obj.rand(0, sl, i, ran))
         * math.cos(obj.rand(0, 360, i, ran) * math.pi / 180)
         * obj.rand(100 - v_ran, 100, i, 1 + ran)
         / 100
-    y = (l * (1 - t) + obj.rand(0, sl, i, ran))
+    local y = (l * (1 - t) + obj.rand(0, sl, i, ran))
         * math.sin(obj.rand(0, 360, i, ran) * math.pi / 180)
         * obj.rand(100 - v_ran, 100, i, 1 + ran)
         / 100
-    zoom = 1 + sv / 100 * (1 - t) - obj.rand(0, s_ran, i, 10 + ran) / 100
-    alpha = 1 - av / 100 * (1 - t)
-    rz = obj.rand(0, 360, i, 6 + ran) * rz_ran + obj.rand(-rzv_ran / 2, rzv_ran / 2, i, 9 + ran) * obj.time
+    local zoom = 1 + sv / 100 * (1 - t) - obj.rand(0, s_ran, i, 10 + ran) / 100
+    local alpha = 1 - av / 100 * (1 - t)
+    local rz = obj.rand(0, 360, i, 6 + ran) * rz_ran + obj.rand(-rzv_ran / 2, rzv_ran / 2, i, 9 + ran) * obj.time
     if zero == 1 and t == 0 then
         alpha = 0
     end
