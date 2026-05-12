265eec7b7c3a259e560e1778e00d81d697aa792c
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-12 16:55:21 +0900

    feat: ループanmを移植

--- scripts/ANM2/ループANM.lua
+++ scripts/ANM2/ループANM.lua
@@ -3,66 +3,84 @@
 ---min=0
 ---max=20
 ---step=0.01
-local t = 1
+local duration = 1
 ---$track:待機[s]
 ---min=0
 ---max=20
 ---step=0.01
-local track1 = 0.2
----$track:拡大+%
----min=-100
----max=800
-local track2 = 50
----$track:縦横比
----min=-100
----max=100
-local track3 = 0
----$value:X
-local x = 0
+local initial_wait = 0.2
 
----$value:Y
-local y = 0
-
----$value:Z
-local z = 0
-
----$value:X軸回転
-local rx = 0
-
----$value:Y軸回転
-local ry = 0
-
----$value:Z軸回転
-local rz = 0
-
----$value:開始位置[s]
+---$track:開始位置[s]
+---min=0
+---max=20
+---step=0.01
 local f = 0
 
 ---$check:待機2回
 local wait = 0
+---$track:X
+---min=-1000
+---max=1000
+local x = 0
 
-w = math.min(track1, t)
-r = 0
-time0 = (math.max(obj.time - f, 0)) % t
+---$track:Y
+---min=-1000
+---max=1000
+local y = 0
+
+---$track:Z
+---min=-1000
+---max=1000
+local z = 0
+--trackgroup@x,y,z:移動
+
+---$track:X軸回転
+---min=-3600
+---max=3600
+local rx = 0
+
+---$track:Y軸回転
+---min=-3600
+---max=3600
+local ry = 0
+
+---$track:Z軸回転
+---min=-3600
+---max=3600
+local rz = 0
+
+--trackgroup@rx,ry,rz:回転
+---$track:拡大+%
+---min=-100
+---max=800
+local scale_offset = 50
+---$track:縦横比
+---min=-100
+---max=100
+local aspect_offset = 0
+
+local w = math.min(initial_wait, duration)
+local r = 0
+local time0 = (math.max(obj.time - f, 0)) % duration
 if wait == 0 then
-    if time0 <= t - w and time0 ~= 0 then
-        r = math.sin(math.pi * time0 / (t - w))
+    if time0 <= duration - w and time0 ~= 0 then
+        r = math.sin(math.pi * time0 / (duration - w))
     end
 else
-    time1 = (math.max(obj.time - f, 0)) % (2 * t)
-    if time1 <= t - w then
-        r = math.sin(math.pi * time0 / (t - w) / 2)
-    elseif time1 <= t then
+    local time1 = (math.max(obj.time - f, 0)) % (2 * duration)
+    if time1 <= duration - w then
+        r = math.sin(math.pi * time0 / (duration - w) / 2)
+    elseif time1 <= duration then
         r = 1
-    elseif time1 <= 2 * t - w then
-        r = 1 - math.sin(math.pi * time0 / (t - w) / 2)
+    elseif time1 <= 2 * duration - w then
+        r = 1 - math.sin(math.pi * time0 / (duration - w) / 2)
     end
 end
 obj.ox = obj.ox + x * r
 obj.oy = obj.oy + y * r
 obj.oz = obj.oz + z * r
-obj.zoom = obj.zoom * (1 + track2 / 100 * r)
-obj.aspect = obj.aspect + track3 / 100 * r
+obj.zoom = obj.zoom * (1 + scale_offset / 100 * r)
+obj.aspect = obj.aspect + aspect_offset / 100 * r
 obj.rx = obj.rx + rx * r
 obj.ry = obj.ry + ry * r
 obj.rz = obj.rz + rz * r
