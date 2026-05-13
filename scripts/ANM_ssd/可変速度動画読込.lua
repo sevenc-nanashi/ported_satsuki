a96264859b7df279d49f430f39062fea42dd6677
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-13 13:08:17 +0900

    feat: 可変速度動画再生を移植

--- scripts/ANM_ssd/可変速度動画読込.lua
+++ scripts/ANM_ssd/可変速度動画読込.lua
@@ -3,34 +3,37 @@
 ---min=0
 ---max=50000
 ---step=1
-local track0 = 0
+local start_frame = 0
 ---$track:再生速度
 ---min=-1000
 ---max=1000
 ---step=1
-local track1 = 100
----$track:ループ
----min=0
----max=1
----step=1
-local track2 = 1
----$track:透明あり
----min=0
----max=1
----step=1
-local track3 = 0
+local playback_speed = 100
+---$check:ループ
+local loop = true
+---$check:透明あり
+local has_alpha = false
 ---$file:ファイル
 local file = ""
-local t = track0 - 1
+
+local _ = playback_speed --未使用警告回避
+
+local frame_position = start_frame - 1
 for i = 0, obj.frame do
-    t = t + obj.getvalue(1, i / 30) / 100
-end
-if track2 == 0 then
-    ksd = t / obj.framerate
-else
-    ksd = (t / obj.framerate) % obj.load("movie", file)
+    frame_position = frame_position + obj.getvalue("track.playback_speed", i / 30) / 100
 end
 
-obj.load("movie", file, ksd, track3)
+local playback_time
+if loop then
+    local frames, rate, scale = obj.load("movie.info", file)
+    if not frames then
+        error("動画ファイルの情報を取得できませんでした。")
+    end
+    playback_time = (frame_position / obj.framerate) % (frames / rate * scale)
+else
+    playback_time = frame_position / obj.framerate
+end
+
+obj.load("movie", file, playback_time, has_alpha)
 
 --再生速度を直線移動等で変化させることができます。
