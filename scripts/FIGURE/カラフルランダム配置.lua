3ece4eb4f15c2f9bd3eec9f298d7a5829a93cdad
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-13 13:36:15 +0900

    feat: カラフルランダム配置を移植

--- scripts/FIGURE/カラフルランダム配置.lua
+++ scripts/FIGURE/カラフルランダム配置.lua
@@ -3,47 +3,68 @@
 ---min=0
 ---max=1000
 ---step=1
-local n = 5
+local count = 5
 ---$track:X幅[%]
 ---min=0
 ---max=5000
-local track1 = 100
+---step=1
+local width_x = 100
 ---$track:Y幅[%]
 ---min=0
 ---max=5000
-local track2 = 100
+---step=1
+local width_y = 100
 ---$track:Z幅[%]
 ---min=0
 ---max=5000
-local track3 = 0
+---step=1
+local width_z = 0
 ---$figure:種類
-local name = "円"
+local figure_name = "円"
 
----$value:サイズ
-local f_size = 100
+---$track:サイズ
+---min=0
+---max=2000
+---step=1
+local figure_size = 100
 
----$value:ライン幅
-local f_line = 4000
+---$track:ライン幅
+---min=0
+---max=10000
+---step=1
+local line_width = 4000
 
----$value:ランダムZ回転
-local rzv_ran = 0
+---$track:ランダムZ回転
+---min=0
+---max=360
+---step=1
+local random_z_rotation_speed = 0
 
----$value:ランダム拡大[-%]
-local s_ran = 0
+---$track:ランダム拡大[-%]
+---min=0
+---max=100
+---step=1
+local random_scale = 0
 
-for i = 0, n do
-    c = (obj.rand(0, 100, i, 100) - 50) / 100
-    if c < 0 then
-        c = math.floor(math.cos(math.pi * c) * 255) * 256 + math.floor(math.sin(math.pi * -c) * 255)
+for i = 0, count do
+    local color_phase = (obj.rand(0, 100, i, 100) - 50) / 100
+    local color
+    if color_phase < 0 then
+        color = math.floor(math.cos(math.pi * color_phase) * 255) * 256
+            + math.floor(math.sin(math.pi * -color_phase) * 255)
     else
-        c = math.floor(math.cos(math.pi * c) * 255) * 256 + math.floor(math.sin(math.pi * c) * 255) * 65536
+        color = math.floor(math.cos(math.pi * color_phase) * 255) * 256
+            + math.floor(math.sin(math.pi * color_phase) * 255) * 65536
     end
-    obj.load("figure", name, c, f_size, f_line)
+
+    obj.load("figure", figure_name, color, figure_size, line_width)
     obj.effect()
-    x = obj.rand(-obj.screen_w, obj.screen_w, i, 0) / 2 * track1 / 100
-    y = obj.rand(-obj.screen_h, obj.screen_h, i, 1) / 2 * track2 / 100
-    z = obj.rand(-1000, 1000, i, 2) / 2 * track3 / 100
-    rz = rand(-100, 100, i, 3) / 100 * obj.time * rzv_ran
-    zoom = 1 - obj.rand(0, s_ran, i, 4) / 100
+
+    local x = obj.rand(-obj.screen_w, obj.screen_w, i, 0) / 2 * width_x / 100
+    local y = obj.rand(-obj.screen_h, obj.screen_h, i, 1) / 2 * width_y / 100
+    local z = obj.rand(-1000, 1000, i, 2) / 2 * width_z / 100
+    local rz = obj.rand(-100, 100, i, 3) / 100 * obj.time * random_z_rotation_speed
+    local zoom = 1 - obj.rand(0, random_scale, i, 4) / 100
+
     obj.draw(x, y, z, zoom, 1, 0, 0, rz)
 end
