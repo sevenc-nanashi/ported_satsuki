c4baa9446d2d5135d5d5a4f0c54de08b8fef16c4
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-13 13:56:42 +0900

    feat: ADC_扇状を移植

--- scripts/加減速/ADC_扇状.lua
+++ scripts/加減速/ADC_扇状.lua
@@ -2,46 +2,47 @@
 ---$track:中心角1
 ---min=0
 ---max=360
-local track1 = 360
+---step=1
+local base_center_angle = 360
 ---$track:中心角2
 ---min=-360
 ---max=360
 ---step=1
-local c2 = 0
+local center_angle_delta = 0
 ---$track:サイズ1
 ---min=0
 ---max=4000
 ---step=1
-local track2 = 100
+local base_size = 100
 ---$track:サイズ2
 ---min=-4000
 ---max=4000
 ---step=1
-local s2 = 0
+local size_delta = 0
 ---$track:ライン幅1
 ---min=0
 ---max=4000
 ---step=1
-local track3 = 4000
+local base_line_width = 4000
 ---$track:ライン幅2
 ---min=-4000
 ---max=4000
 ---step=1
-local h2 = 0
+local line_width_delta = 0
 ---$color:色
-local col = 0xffffff
+local color = 0xffffff
 
 --separator:加減速
 ---$track:時間[s]
 ---min=-5
 ---max=5
 ---step=0.01
-local ta = 1
+local duration = 1
 ---$track:加減速
 ---min=1
 ---max=5
 ---step=1
-local beki = 2
+local easing_power = 2
 ---$select:モード
 ---減速=0
 ---加速=1
@@ -49,34 +50,35 @@ local beki = 2
 local mode = 0
 
 --共通部分
-local t
-if ta == 0 then
+local progress
+if duration == 0 then
     return
-elseif ta < 0 then
-    t = (ta - obj.time + obj.totaltime) / ta
+elseif duration < 0 then
+    progress = (duration - obj.time + obj.totaltime) / duration
 else
-    t = (ta - obj.time) / ta
+    progress = (duration - obj.time) / duration
 end
-t = math.max(0, t)
+progress = math.max(0, progress)
 
 if mode < 1 then
-    t = t ^ beki
+    progress = progress ^ easing_power
 elseif mode < 2 then
-    t = 1 - (1 - t) ^ beki
+    progress = 1 - (1 - progress) ^ easing_power
 else
-    if t <= 0.5 then
-        t = (2 * t) ^ beki / 2
+    if progress <= 0.5 then
+        progress = (2 * progress) ^ easing_power / 2
     else
-        t = (1 - (1 - (t - 0.5) * 2) ^ beki) / 2 + 0.5
+        progress = (1 - (1 - (progress - 0.5) * 2) ^ easing_power) / 2 + 0.5
     end
 end
 
 --フィルタ効果(「扇状」を改変)
-local c = track1 + c2 * t
-local s = track2 + s2 * t
-local h = track3 + h2 * t
-local l = math.floor(s / 2)
-obj.load("figure", "円", col, l * 2, h)
-obj.effect("斜めクリッピング", "角度", 180 - c / 2)
-obj.effect("ミラー", "境目調整", -l)
-obj.rz = -c / 2 - 90
+local center_angle = base_center_angle + center_angle_delta * progress
+local size = base_size + size_delta * progress
+local line_width = base_line_width + line_width_delta * progress
+local radius = math.floor(size / 2)
+
+obj.load("figure", "円", color, radius * 2, line_width)
+obj.effect("斜めクリッピング", "角度", 180 - center_angle / 2)
+obj.effect("ミラー", "境目調整", -radius)
+obj.rz = -center_angle / 2 - 90
