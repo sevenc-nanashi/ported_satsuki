f1d7e4eb959948881131e3375c161bce4df71076
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-13 13:46:53 +0900

    feat: セピアノイズを移植

--- scripts/FIGURE/セピアノイズ.lua
+++ scripts/FIGURE/セピアノイズ.lua
@@ -2,97 +2,111 @@
 ---$track:ゴミ閾値
 ---min=0
 ---max=100
-local gs = 90
+---step=1
+local dust_threshold = 90
 ---$track:ゴミサイズ
 ---min=0
 ---max=200
-local track1 = 100
+---step=1
+local dust_size = 100
 ---$track:縦線閾値
 ---min=0
 ---max=100
-local ts = 90
+---step=1
+local vertical_line_threshold = 90
 ---$track:輝度変化
 ---min=0
 ---max=100
-local kh = 90
+---step=1
+local brightness_variation = 90
 ---$color:縁色
-local color = 0x6b4a2b
+local edge_color = 0x6b4a2b
 
 ---$color:ノイズ色
-local ncolor = 0x000000
+local noise_color = 0x000000
 
 ---$check:縁アス比
-local as = 0
+local edge_aspect_ratio = false
 
 ---$check:縁あり
-local ed = 1
+local show_edge = true
 
 ---$check:輝度反転
-local ha = 0
+local invert_brightness = false
 
-gb = track1 / 100
+local dust_scale = dust_size / 100
 
 --白背景
 obj.load("figure", "背景", 0xffffff)
-obj.effect("色調補正", "輝度", obj.rand(kh, 100))
+obj.effect("色調補正", "輝度", obj.rand(brightness_variation, 100))
 obj.setoption("dst", "tmp", obj.screen_w, obj.screen_h)
 obj.draw()
 
 --色付き背景
-obj.load("figure", "背景", color)
+obj.load("figure", "背景", edge_color)
 obj.setoption("dst", "tmp")
 obj.draw(0, 0, 0, 1, 0.2)
 
 --縁
-if ed == 1 then
-    ss = 50
-    obj.load("figure", "四角形", color, ss * 2)
-    obj.effect("マスク", "type", 1, "サイズ", ss, "マスクの反転", 1)
+if show_edge then
+    local edge_size = 50
+    obj.load("figure", "四角形", edge_color, edge_size * 2)
+    obj.effect("マスク", "type", 1, "サイズ", edge_size, "マスクの反転", 1)
     obj.effect("ぼかし", "範囲", 10, "サイズ固定", 1)
-    w = obj.screen_w / 2
-    h = w
-    if as == 1 then
-        h = obj.screen_h / 2
+    local edge_width = obj.screen_w / 2
+    local edge_height = edge_width
+    if edge_aspect_ratio then
+        edge_height = obj.screen_h / 2
     end
     obj.setoption("dst", "tmp")
     obj.drawpoly(
-        -w,
-        -h,
+        -edge_width,
+        -edge_height,
         0,
-        w,
-        -h,
+        edge_width,
+        -edge_height,
         0,
-        w,
-        h,
+        edge_width,
+        edge_height,
         0,
-        -w,
-        h,
+        -edge_width,
+        edge_height,
         0,
-        ss / 2,
-        ss / 2,
-        ss * 3 / 2,
-        ss / 2,
-        ss * 3 / 2,
-        ss * 3 / 2,
-        ss / 2,
-        ss * 3 / 2
+        edge_size / 2,
+        edge_size / 2,
+        edge_size * 3 / 2,
+        edge_size / 2,
+        edge_size * 3 / 2,
+        edge_size * 3 / 2,
+        edge_size / 2,
+        edge_size * 3 / 2
     )
 end
 
 --ゴミノイズ
-obj.load("figure", "四角形", ncolor, obj.screen_w)
-obj.effect("ノイズ", "変化速度", 50, "周期X", 10 * gb, "周期Y", 10 * gb, "しきい値", gs)
-x = rand(-obj.screen_w / 2, obj.screen_w / 2)
-y = rand(-obj.screen_h / 2, obj.screen_h / 2)
+obj.load("figure", "四角形", noise_color, obj.screen_w)
+obj.effect(
+    "ノイズ",
+    "変化速度",
+    50,
+    "周期X",
+    10 * dust_scale,
+    "周期Y",
+    10 * dust_scale,
+    "しきい値",
+    dust_threshold
+)
+local dust_x = obj.rand(-obj.screen_w / 2, obj.screen_w / 2)
+local dust_y = obj.rand(-obj.screen_h / 2, obj.screen_h / 2)
 obj.setoption("dst", "tmp")
-obj.draw(x, y, 0, 2)
+obj.draw(dust_x, dust_y, 0, 2)
 
 --縦線ノイズ
-obj.load("figure", "四角形", ncolor, obj.screen_w)
-obj.effect("ノイズ", "変化速度", 50, "周期X", 20, "周期Y", 0.01, "しきい値", ts)
+obj.load("figure", "四角形", noise_color, obj.screen_w)
+obj.effect("ノイズ", "変化速度", 50, "周期X", 20, "周期Y", 0.01, "しきい値", vertical_line_threshold)
 obj.setoption("dst", "tmp")
 obj.draw()
 
 --描画
 obj.load("tempbuffer")
-obj.effect("反転", "輝度反転", ha)
+obj.effect("反転", "輝度反転", invert_brightness)
