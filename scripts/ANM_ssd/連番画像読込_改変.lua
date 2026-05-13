96f7e937355eaceeae870173de5cb96b6ae4da81
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-13 13:13:17 +0900

    feat: 連番画像読込_改変を移植

--- scripts/ANM_ssd/連番画像読込_改変.lua
+++ scripts/ANM_ssd/連番画像読込_改変.lua
@@ -3,31 +3,37 @@
 ---min=1
 ---max=8
 ---step=1
-local kt = 4
+local digit_count = 4
 ---$track:再生速度
 ---min=0
 ---max=1000
-local track1 = 100
+---step=1
+local playback_speed = 100
 ---$track:開始位置
 ---min=0
 ---max=10000
 ---step=1
-local track2 = 0
+local start_frame = 0
 ---$track:ループ位置
 ---min=0
 ---max=10000
 ---step=1
-local track3 = 10000
+local loop_frame = 10000
 ---$file:ファイル
 local file = ""
 
 --連番画像読み込み(hksy.obj)を改変したものです。
 --ファイル名は(0001.png)などの連番数字が前提
-lp = string.len(file) - (kt + 4)
-path = string.sub(file, 1, lp)
-ext = string.sub(file, -3)
-frame = obj.time * obj.framerate
-num = (frame * (track1 / 100)) % (track3 - track2 + 1) + track2
-name = string.format("%0" .. kt .. "d", num) .. "." .. ext
-load = path .. name
-obj.load("image", load)
+local base_path, extension = string.match(file, "^(.*)%.([^%.\\/]*)$")
+
+if base_path ~= nil then
+    local path = string.sub(base_path, 1, string.len(base_path) - digit_count)
+    local frame = obj.time * obj.framerate
+    local image_number = (frame * (playback_speed / 100)) % (loop_frame - start_frame + 1) + start_frame
+    local image_name = string.format("%0" .. digit_count .. "d", image_number) .. "." .. extension
+    local image_path = path .. image_name
+
+    obj.load("image", image_path)
+else
+    obj.load("image", file)
+end
