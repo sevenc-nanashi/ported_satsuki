e7933cdff7d331f09fb57d94a82034188ec757af
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-13 13:52:15 +0900

    feat: ADC_図形を移植

--- scripts/加減速/ADC_図形.lua
+++ scripts/加減速/ADC_図形.lua
@@ -3,39 +3,39 @@
 ---min=0
 ---max=4000
 ---step=1
-local track1 = 100
+local base_size = 100
 ---$track:サイズ2
 ---min=-4000
 ---max=4000
 ---step=1
-local track2 = 0
+local size_delta = 0
 ---$track:ライン幅1
 ---min=0
 ---max=4000
 ---step=1
-local track3 = 4000
+local base_line_width = 4000
 ---$track:ライン幅2
 ---min=-4000
 ---max=4000
 ---step=1
-local l2 = 0
+local line_width_delta = 0
 ---$figure:種類
-local fig = "円"
+local figure_name = "円"
 
 ---$color:色
-local col = 0xffffff
+local color = 0xffffff
 
 --separator:加減速
 ---$track:時間[s]
 ---min=-5
 ---max=5
 ---step=0.01
-local ta = 1
+local duration = 1
 ---$track:加減速
 ---min=1
 ---max=5
 ---step=1
-local beki = 2
+local easing_power = 2
 ---$select:モード
 ---減速=0
 ---加速=1
@@ -43,29 +43,30 @@ local beki = 2
 local mode = 0
 
 --共通部分
-local t
-if ta == 0 then
+local progress
+if duration == 0 then
     return
-elseif ta < 0 then
-    t = (ta - obj.time + obj.totaltime) / ta
+elseif duration < 0 then
+    progress = (duration - obj.time + obj.totaltime) / duration
 else
-    t = (ta - obj.time) / ta
+    progress = (duration - obj.time) / duration
 end
-t = math.max(0, t)
+progress = math.max(0, progress)
 
 if mode < 1 then
-    t = t ^ beki
+    progress = progress ^ easing_power
 elseif mode < 2 then
-    t = 1 - (1 - t) ^ beki
+    progress = 1 - (1 - progress) ^ easing_power
 else
-    if t <= 0.5 then
-        t = (2 * t) ^ beki / 2
+    if progress <= 0.5 then
+        progress = (2 * progress) ^ easing_power / 2
     else
-        t = (1 - (1 - (t - 0.5) * 2) ^ beki) / 2 + 0.5
+        progress = (1 - (1 - (progress - 0.5) * 2) ^ easing_power) / 2 + 0.5
     end
 end
 
 --フィルタ効果
-local s = track1 + track2 * t
-local l = track3 + l2 * t
-obj.load("figure", fig, col, s, l)
+local size = base_size + size_delta * progress
+local line_width = base_line_width + line_width_delta * progress
+
+obj.load("figure", figure_name, color, size, line_width)
