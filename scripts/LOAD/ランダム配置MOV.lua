986dce7dcabd3530949462fdacbc61abe52eb1c2
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-13 12:22:18 +0900

    feat: ランダム配置movを移植

--- scripts/LOAD/ランダム配置MOV.lua
+++ scripts/LOAD/ランダム配置MOV.lua
@@ -7,24 +7,28 @@ local n = 5
 ---$track:再生速度
 ---min=0
 ---max=800
-local track1 = 100
+---step=1
+local playback_speed = 100
 ---$track:再生位置
 ---min=0
 ---max=300
 ---step=0.01
-local f = 0
+local playback_position = 0
 ---$track:配置範囲
 ---min=0
 ---max=1000
-local track3 = 100
---file:
-v = track1 / 100
-l = track3 / 100
-tt = obj.load(file)
+---step=1
+local placement_range = 100
+---$file:動画ファイル
+local file = ""
+
+local speed = playback_speed / 100
+local range = placement_range / 100
+local duration = obj.load(file)
 for i = 0, n - 1 do
-    x = obj.rand(-obj.screen_w, obj.screen_w, i, 0) / 2 * l
-    y = obj.rand(-obj.screen_h, obj.screen_h, i, 1) / 2 * l
-    obj.load(file, (f + tt * i / n + obj.time * v) % tt)
+    local x = obj.rand(-obj.screen_w, obj.screen_w, i, 0) / 2 * range
+    local y = obj.rand(-obj.screen_h, obj.screen_h, i, 1) / 2 * range
+    obj.load(file, (playback_position + duration * i / n + obj.time * speed) % duration)
     obj.effect()
     obj.draw(x, y, 0, 1, 1, 0, 0, obj.rand(0, 360, i, 2))
 end
