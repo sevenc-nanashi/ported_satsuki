41e3a137493759dc94d26d6390ed25d308bf513c
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-13 14:05:39 +0900

    feat: 集中線Plusを移植

--- scripts/ANM_ssd/集中線plus.lua
+++ scripts/ANM_ssd/集中線plus.lua
@@ -2,75 +2,132 @@
 ---$track:線％
 ---min=0.1
 ---max=10
-local vb = 1
+---step=0.1
+local line_percent = 1
 ---$track:空き％
 ---min=1
 ---max=20
-local va = 5
+---step=0.1
+local gap_percent = 5
 ---$track:中心
 ---min=0
 ---max=800
-local lc = 100
+---step=1
+local center_radius = 100
 ---$track:回転
 ---min=-720
 ---max=720
-local r = 0
+---step=1
+local rotation = 0
 ---$color:色
-local col = 0xffffff
+local color = 0xffffff
 
----$value:横サイズ
-local w = 800
+---$track:横サイズ
+---min=1
+---max=4000
+---step=1
+local width = 800
 
----$value:縦サイズ
-local h = 450
+---$track:縦サイズ
+---min=1
+---max=4000
+---step=1
+local height = 450
 
----$value:中心座標
-local pos = { 0, 0 }
+---$track:中心座標X
+---min=-4000
+---max=4000
+---step=1
+local center_x = 0
+---$track:中心座標Y
+---min=-4000
+---max=4000
+---step=1
+local center_y = 0
 
----$value:中心ランダム％
-local lc_r = 20
+--trackgroup@center_x,center_y:中心座標
 
----$value:ランダム種
-local rseed = 0
+---$track:中心ランダム％
+---min=0
+---max=100
+---step=1
+local center_random_percent = 20
 
----$value:切替ﾌﾚｰﾑ(0
-local kfr = 0
+---$track:ランダム種
+---min=0
+---max=10000
+---step=1
+local random_seed = 0
 
-obj.setanchor("pos", 1)
-local cx = pos[1]
-local cy = pos[2]
-local l = math.sqrt((w + math.abs(cx)) ^ 2 + (h + math.abs(cy)) ^ 2) * 1.2
+---$track:切替フレーム
+---min=0
+---max=10000
+---step=1
+---zero_label=なし
+local switch_frame = 0
 
-obj.setoption("drawtarget", "tempbuffer", w, h)
-obj.load("figure", "四角形", col, 2)
+obj.setanchor("center_x,center_y", 1)
+local line_length = math.sqrt((width + math.abs(center_x)) ^ 2 + (height + math.abs(center_y)) ^ 2) * 1.2
 
-local a = 0
-local b = 0
+obj.setoption("drawtarget", "tempbuffer", width, height)
+obj.load("figure", "四角形", color, 2)
+local figure_w = obj.w
+local figure_h = obj.h
+
+local start_percent = 0
+local end_percent = 0
 local i = 1
+local vertices = {}
 
-while b <= 100 do
-    if kfr == 0 then
-        fr_num = 0
+while end_percent <= 100 do
+    local frame_number
+    if switch_frame == 0 then
+        frame_number = 0
     else
-        fr_num = math.floor(obj.frame / kfr)
+        frame_number = math.floor(obj.frame / switch_frame)
     end
 
-    a = b + obj.rand(0, 100, i + math.floor(rseed), fr_num) / 100 * va
-    b = a + obj.rand(0, 100, i + 1 + math.floor(rseed), fr_num) / 100 * vb
-    llc = lc * (1 + obj.rand(0, 100, i + 2 + math.floor(rseed), fr_num) / 100 * lc_r / 100)
+    local seed = math.floor(random_seed)
+    start_percent = end_percent + obj.rand(0, 100, i + seed, frame_number) / 100 * gap_percent
+    end_percent = start_percent + obj.rand(0, 100, i + 1 + seed, frame_number) / 100 * line_percent
+    local randomized_center_radius = center_radius
+        * (1 + obj.rand(0, 100, i + 2 + seed, frame_number) / 100 * center_random_percent / 100)
 
-    if a <= 100 and b <= 100 then
-        x0 = cx + llc * math.cos(math.rad(r + 360 * (a + b) / 2 / 100))
-        y0 = cy + llc * math.sin(math.rad(r + 360 * (a + b) / 2 / 100))
-        x1 = cx + l * math.cos(math.rad(r + 360 * a / 100))
-        y1 = cy + l * math.sin(math.rad(r + 360 * a / 100))
-        x2 = cx + l * math.cos(math.rad(r + 360 * (a + b) / 2 / 100))
-        y2 = cy + l * math.sin(math.rad(r + 360 * (a + b) / 2 / 100))
-        x3 = cx + l * math.cos(math.rad(r + 360 * b / 100))
-        y3 = cy + l * math.sin(math.rad(r + 360 * b / 100))
-        obj.drawpoly(x0, y0, 0, x1, y1, 0, x2, y2, 0, x3, y3, 0)
+    if start_percent <= 100 and end_percent <= 100 then
+        local middle_percent = (start_percent + end_percent) / 2
+        local x0 = center_x + randomized_center_radius * math.cos(math.rad(rotation + 360 * middle_percent / 100))
+        local y0 = center_y + randomized_center_radius * math.sin(math.rad(rotation + 360 * middle_percent / 100))
+        local x1 = center_x + line_length * math.cos(math.rad(rotation + 360 * start_percent / 100))
+        local y1 = center_y + line_length * math.sin(math.rad(rotation + 360 * start_percent / 100))
+        local x2 = center_x + line_length * math.cos(math.rad(rotation + 360 * middle_percent / 100))
+        local y2 = center_y + line_length * math.sin(math.rad(rotation + 360 * middle_percent / 100))
+        local x3 = center_x + line_length * math.cos(math.rad(rotation + 360 * end_percent / 100))
+        local y3 = center_y + line_length * math.sin(math.rad(rotation + 360 * end_percent / 100))
+        vertices[#vertices + 1] = {
+            x0,
+            y0,
+            0,
+            x1,
+            y1,
+            0,
+            x2,
+            y2,
+            0,
+            x3,
+            y3,
+            0,
+            0,
+            0,
+            figure_w,
+            0,
+            figure_w,
+            figure_h,
+            0,
+            figure_h,
+        }
     end
     i = i + 1
 end
 
+obj.drawpoly(vertices)
 obj.load("tempbuffer")
