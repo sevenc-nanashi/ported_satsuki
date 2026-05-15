b3308bafd143fa7480ecd457633cd2eaa2584907
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-16 08:13:01 +0900

    feat: 伸縮登場を移植

--- scripts/ANM1/伸縮登場.lua
+++ scripts/ANM1/伸縮登場.lua
@@ -3,73 +3,79 @@
 ---min=-5
 ---max=5
 ---step=0.01
-local track0 = 0.3
+local duration = 0.3
 ---$track:変化増%
 ---min=-100
 ---max=100
-local v = 0
----$value:X軸1
-local xa = 40
+local variation = 0
+---$track:X軸1
+---min=0
+---max=500
+local x_scale1 = 40
 
----$value:X軸2
-local xb = 150
+---$track:X軸2
+---min=0
+---max=500
+local x_scale2 = 150
 
----$value:Y軸1
-local ya = 150
+---$track:Y軸1
+---min=0
+---max=500
+local y_scale1 = 150
 
----$value:Y軸2
-local yb = 80
+---$track:Y軸2
+---min=0
+---max=500
+local y_scale2 = 80
 
-t = track0 / 3
-xa = xa + (xa - 100) * v / 100
-xb = xb + (xb - 100) * v / 100
-ya = ya + (ya - 100) * v / 100
-yb = yb + (yb - 100) * v / 100
+local part_duration = duration / 3
+x_scale1 = x_scale1 + (x_scale1 - 100) * variation / 100
+x_scale2 = x_scale2 + (x_scale2 - 100) * variation / 100
+y_scale1 = y_scale1 + (y_scale1 - 100) * variation / 100
+y_scale2 = y_scale2 + (y_scale2 - 100) * variation / 100
 
-if t == 0 then
+local x_scale
+local y_scale
+if part_duration == 0 then
     return
-elseif t > 0 then
-    if obj.time < t then
-        ta = (t - obj.time) / t
-        xscale = 0 + (xa - 0) * (1 - ta)
-        yscale = 0 + (ya - 0) * (1 - ta)
-    elseif obj.time < 2 * t then
-        tb = (t - (obj.time - t)) / t
-        xscale = xa + (xb - xa) * (1 - tb)
-        yscale = ya + (yb - ya) * (1 - tb)
-    elseif obj.time < 3 * t then
-        tc = (t - (obj.time - 2 * t)) / t
-        xscale = xb + (100 - xb) * (1 - tc)
-        yscale = yb + (100 - yb) * (1 - tc)
+elseif part_duration > 0 then
+    if obj.time < part_duration then
+        local progress = 1 - (part_duration - obj.time) / part_duration
+        x_scale = x_scale1 * progress
+        y_scale = y_scale1 * progress
+    elseif obj.time < 2 * part_duration then
+        local progress = 1 - (part_duration - (obj.time - part_duration)) / part_duration
+        x_scale = x_scale1 + (x_scale2 - x_scale1) * progress
+        y_scale = y_scale1 + (y_scale2 - y_scale1) * progress
+    elseif obj.time < 3 * part_duration then
+        local progress = 1 - (part_duration - (obj.time - 2 * part_duration)) / part_duration
+        x_scale = x_scale2 + (100 - x_scale2) * progress
+        y_scale = y_scale2 + (100 - y_scale2) * progress
     else
-        xscale = 100
-        yscale = 100
+        x_scale = 100
+        y_scale = 100
     end
 else
-    t = math.abs(t)
-    if obj.time > obj.totaltime - 3 * t then
-        if obj.time < obj.totaltime - 2 * t then
-            tc = (t - (obj.time - obj.totaltime + 3 * t)) / t
-            xscale = 100 + (xb - 100) * (1 - tc)
-            yscale = 100 + (yb - 100) * (1 - tc)
-        elseif obj.time < obj.totaltime - t then
-            tb = (t - (obj.time - obj.totaltime + 2 * t)) / t
-            xscale = xb + (xa - xb) * (1 - tb)
-            yscale = yb + (ya - yb) * (1 - tb)
+    part_duration = math.abs(part_duration)
+    if obj.time > obj.totaltime - 3 * part_duration then
+        if obj.time < obj.totaltime - 2 * part_duration then
+            local progress = 1 - (part_duration - (obj.time - obj.totaltime + 3 * part_duration)) / part_duration
+            x_scale = 100 + (x_scale2 - 100) * progress
+            y_scale = 100 + (y_scale2 - 100) * progress
+        elseif obj.time < obj.totaltime - part_duration then
+            local progress = 1 - (part_duration - (obj.time - obj.totaltime + 2 * part_duration)) / part_duration
+            x_scale = x_scale2 + (x_scale1 - x_scale2) * progress
+            y_scale = y_scale2 + (y_scale1 - y_scale2) * progress
         else
-            ta = (t - (obj.time - obj.totaltime + t)) / t
-            xscale = xa + (0 - xa) * (1 - ta)
-            yscale = ya + (0 - ya) * (1 - ta)
+            local progress = 1 - (part_duration - (obj.time - obj.totaltime + part_duration)) / part_duration
+            x_scale = x_scale1 * (1 - progress)
+            y_scale = y_scale1 * (1 - progress)
         end
     else
-        xscale = 100
-        yscale = 100
+        x_scale = 100
+        y_scale = 100
     end
 end
 
-obj.zoom = math.max(xscale, yscale) / 100
-if xscale > yscale then
-    obj.aspect = yscale / xscale - 1
-else
-    obj.aspect = 1 - xscale / yscale
-end
+obj.sx = x_scale / 100
+obj.sy = y_scale / 100
