b5e88948cda24dd54509d722a5aaeba3a70e18de
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-16 14:59:49 +0900

    feat: 矢がすりを移植

--- scripts/ANM_ssd/矢がすり.lua
+++ scripts/ANM_ssd/矢がすり.lua
@@ -2,186 +2,196 @@
 ---$track:横幅
 ---min=1
 ---max=500
-local ww = 15
+local arrow_width = 15
 ---$track:長さ
 ---min=1
 ---max=500
-local hh = 65
+local arrow_length = 65
 ---$track:横サイズ
----min=0
+---min=1
 ---max=4000
-local w = 800
+local width = 800
 ---$track:縦サイズ
----min=0
+---min=1
 ---max=4000
-local h = 450
----$value:ライン幅
-local lhb = 2
+local height = 450
+---$track:ライン幅
+---min=0
+---max=500
+local line_width = 2
 
 ---$color:色1
-local col1 = 0x000000
+local foreground_color = 0x000000
 
----$color:色2(負値で透明)
-local col2 = 0xffffff
+---$color:色2
+local background_color = nil
 
 ---$check:タイプ2
-local uuss = 1
+local alternate_type = true
 
-obj.setoption("drawtarget", "tempbuffer", w, h)
+obj.setoption("drawtarget", "tempbuffer", width, height)
 
---背景
-if col2 >= 0 then
-    obj.load("figure", "四角形", col2, 2)
-    obj.drawpoly(-w / 2, -h / 2, 0, w / 2, -h / 2, 0, w / 2, h / 2, 0, -w / 2, h / 2, 0)
+-- 背景
+if background_color then
+    obj.load("figure", "四角形", background_color, 2)
+    obj.drawpoly(
+        -width / 2,
+        -height / 2,
+        0,
+        width / 2,
+        -height / 2,
+        0,
+        width / 2,
+        height / 2,
+        0,
+        -width / 2,
+        height / 2,
+        0
+    )
 end
 
---模様
-obj.load("figure", "四角形", col1, 2)
+-- 模様
+obj.load("figure", "四角形", foreground_color, 2)
 
-local nx = math.floor(w / (ww * 4 + lhb * 2)) + 1
-local ny = math.floor(h / hh) + 1
+local vertices = {}
 
-for j = -3, ny do
-    y = -h / 2 + hh * j
-    for i = 0, nx do
-        if j % 2 == 0 then
-            x = -w / 2 + (ww * 4 + lhb * 2) * i
+local function add_quad(x1, y1, x2, y2, x3, y3, x4, y4)
+    vertices[#vertices + 1] = {
+        x1,
+        y1,
+        0,
+        x2,
+        y2,
+        0,
+        x3,
+        y3,
+        0,
+        x4,
+        y4,
+        0,
+        0,
+        0,
+        1,
+        0,
+        1,
+        1,
+        0,
+        1,
+    }
+end
+
+local x_period = arrow_width * 4 + line_width * 2
+local column_count = math.floor(width / x_period) + 1
+local row_count = math.floor(height / arrow_length) + 1
+
+for row = -3, row_count do
+    local y = -height / 2 + arrow_length * row
+    for column = 0, column_count do
+        local x
+        if row % 2 == 0 then
+            x = -width / 2 + x_period * column
         else
-            x = -w / 2 + (ww * 4 + lhb * 2) * i + ww * 2 + lhb
+            x = -width / 2 + x_period * column + arrow_width * 2 + line_width
         end
-        if uuss == 0 then
-            obj.drawpoly(
-                x - ww - lhb / 2,
-                y + ww,
-                0,
-                x - lhb / 2,
+
+        if not alternate_type then
+            add_quad(
+                x - arrow_width - line_width / 2,
+                y + arrow_width,
+                x - line_width / 2,
                 y,
-                0,
-                x - lhb / 2,
-                y + hh,
-                0,
-                x - ww - lhb / 2,
-                y + ww + hh,
-                0
+                x - line_width / 2,
+                y + arrow_length,
+                x - arrow_width - line_width / 2,
+                y + arrow_width + arrow_length
             ) --上向（左）
-            obj.drawpoly(
-                x + lhb / 2,
+            add_quad(
+                x + line_width / 2,
                 y,
-                0,
-                x + ww + lhb / 2,
-                y + ww,
-                0,
-                x + ww + lhb / 2,
-                y + ww + hh,
-                0,
-                x + lhb / 2,
-                y + hh,
-                0
+                x + arrow_width + line_width / 2,
+                y + arrow_width,
+                x + arrow_width + line_width / 2,
+                y + arrow_width + arrow_length,
+                x + line_width / 2,
+                y + arrow_length
             ) --上向（右）
-            obj.drawpoly(
-                x + lhb / 2 + ww * 2,
+            add_quad(
+                x + line_width / 2 + arrow_width * 2,
                 y,
-                0,
-                x + lhb / 2 + ww * 2 + lhb,
+                x + line_width / 2 + arrow_width * 2 + line_width,
                 y,
-                0,
-                x + lhb / 2 + ww * 2 + lhb,
-                y + hh,
-                0,
-                x + lhb / 2 + ww * 2,
-                y + hh,
-                0
+                x + line_width / 2 + arrow_width * 2 + line_width,
+                y + arrow_length,
+                x + line_width / 2 + arrow_width * 2,
+                y + arrow_length
             ) --ライン
         else
-            if i % 2 == 0 then
-                obj.drawpoly(
-                    x - ww - lhb / 2,
-                    y + ww,
-                    0,
-                    x - lhb / 2,
+            if column % 2 == 0 then
+                add_quad(
+                    x - arrow_width - line_width / 2,
+                    y + arrow_width,
+                    x - line_width / 2,
                     y,
-                    0,
-                    x - lhb / 2,
-                    y + hh,
-                    0,
-                    x - ww - lhb / 2,
-                    y + ww + hh,
-                    0
+                    x - line_width / 2,
+                    y + arrow_length,
+                    x - arrow_width - line_width / 2,
+                    y + arrow_width + arrow_length
                 ) --上向（左）
-                obj.drawpoly(
-                    x + lhb / 2,
+                add_quad(
+                    x + line_width / 2,
                     y,
-                    0,
-                    x + ww + lhb / 2,
-                    y + ww,
-                    0,
-                    x + ww + lhb / 2,
-                    y + ww + hh,
-                    0,
-                    x + lhb / 2,
-                    y + hh,
-                    0
+                    x + arrow_width + line_width / 2,
+                    y + arrow_width,
+                    x + arrow_width + line_width / 2,
+                    y + arrow_width + arrow_length,
+                    x + line_width / 2,
+                    y + arrow_length
                 ) --上向（右）
-                obj.drawpoly(
-                    x - lhb / 2,
-                    y + hh,
-                    0,
-                    x + lhb / 2,
-                    y + hh,
-                    0,
-                    x + lhb / 2,
-                    y + hh * 2,
-                    0,
-                    x - lhb / 2,
-                    y + hh * 2,
-                    0
+                add_quad(
+                    x - line_width / 2,
+                    y + arrow_length,
+                    x + line_width / 2,
+                    y + arrow_length,
+                    x + line_width / 2,
+                    y + arrow_length * 2,
+                    x - line_width / 2,
+                    y + arrow_length * 2
                 ) --ライン
             else
-                obj.drawpoly(
-                    x - ww - lhb / 2,
-                    y + ww,
-                    0,
-                    x - lhb / 2,
-                    y + ww * 2,
-                    0,
-                    x - lhb / 2,
-                    y + hh + ww * 2,
-                    0,
-                    x - ww - lhb / 2,
-                    y + ww + hh,
-                    0
+                add_quad(
+                    x - arrow_width - line_width / 2,
+                    y + arrow_width,
+                    x - line_width / 2,
+                    y + arrow_width * 2,
+                    x - line_width / 2,
+                    y + arrow_length + arrow_width * 2,
+                    x - arrow_width - line_width / 2,
+                    y + arrow_width + arrow_length
                 ) --下向（左）
-                obj.drawpoly(
-                    x + lhb / 2,
-                    y + ww * 2,
-                    0,
-                    x + ww + lhb / 2,
-                    y + ww,
-                    0,
-                    x + ww + lhb / 2,
-                    y + ww + hh,
-                    0,
-                    x + lhb / 2,
-                    y + hh + ww * 2,
-                    0
+                add_quad(
+                    x + line_width / 2,
+                    y + arrow_width * 2,
+                    x + arrow_width + line_width / 2,
+                    y + arrow_width,
+                    x + arrow_width + line_width / 2,
+                    y + arrow_width + arrow_length,
+                    x + line_width / 2,
+                    y + arrow_length + arrow_width * 2
                 ) --下向（右）
-                obj.drawpoly(
-                    x - lhb / 2,
-                    y + ww * 2 - hh,
-                    0,
-                    x + lhb / 2,
-                    y + ww * 2 - hh,
-                    0,
-                    x + lhb / 2,
-                    y + ww * 2,
-                    0,
-                    x - lhb / 2,
-                    y + ww * 2,
-                    0
+                add_quad(
+                    x - line_width / 2,
+                    y + arrow_width * 2 - arrow_length,
+                    x + line_width / 2,
+                    y + arrow_width * 2 - arrow_length,
+                    x + line_width / 2,
+                    y + arrow_width * 2,
+                    x - line_width / 2,
+                    y + arrow_width * 2
                 ) --ライン
             end
         end
     end
 end
 
+obj.drawpoly(vertices)
 obj.load("tempbuffer")
