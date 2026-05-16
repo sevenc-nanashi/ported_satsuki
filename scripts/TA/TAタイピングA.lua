f7b769ab4e48d1a8eb7461652d3f7eaf8bd3d3ac
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-16 15:26:26 +0900

    feat: TAタイピングABを移植

--- scripts/TA/TAタイピングA.lua
+++ scripts/TA/TAタイピングA.lua
@@ -1,16 +1,17 @@
 --label:${ROOT_CATEGORY}\切り替え効果\@TA
 ---$track:表示速度
----min=0
+---min=1
 ---max=100
-local track0 = 5
-v = 1 / track0 --単位：秒/文字
-n = obj.num
-if obj.index == 0 then
-    if tp == null then
-        tp = {}
-    end
+local display_speed = 5
+
+local character_interval = 1 / display_speed -- 単位：秒/文字
+if obj.index == 0 or S_ta_typing_positions == nil then
+    S_ta_typing_positions = {}
 end
-tp[obj.index + 1] = { obj.ox, obj.oy, obj.w, obj.h }
-if obj.time < obj.index * v + v then
+S_ta_typing_interval = character_interval
+S_ta_typing_count = obj.num
+S_ta_typing_positions[obj.index + 1] = { obj.ox, obj.oy, obj.w, obj.h }
+
+if obj.time < obj.index * character_interval + character_interval then
     obj.alpha = 0
 end
--- scripts/TA/TAタイピングB.lua
+++ scripts/TA/TAタイピングB.lua
@@ -2,38 +2,53 @@
 ---$track:-横幅
 ---min=0
 ---max=256
-local track0 = 0
+local cursor_width_reduction = 0
 ---$track:太さ
 ---min=0
 ---max=256
-local track1 = 1
+local cursor_thickness = 1
 ---$color:色
-local color = 0xffffff
+local cursor_color = 0xffffff
 
-obj.effect()
-if obj.index <= n - 1 then
-    for i = 0, n - 1 do
-        if obj.index == i then
-            obj.ox = tp[i + 1][1] - tp[i + 1][3] / 2 + obj.w / 2
-            obj.oy = tp[i + 1][2]
-            if obj.time < v * (i + 0.5) or obj.time >= v * (i + 1) then
+local positions = S_ta_typing_positions
+local character_interval = S_ta_typing_interval
+local character_count = S_ta_typing_count
+
+if positions == nil or character_interval == nil or character_count == nil then
+    obj.alpha = 0
+else
+    obj.effect()
+    if obj.index <= character_count - 1 then
+        local position = positions[obj.index + 1]
+        if position then
+            obj.ox = position[1] - position[3] / 2 + obj.w / 2
+            obj.oy = position[2]
+            if
+                obj.time < character_interval * (obj.index + 0.5)
+                or obj.time >= character_interval * (obj.index + 1)
+            then
                 obj.alpha = 0
             end
+        else
+            obj.alpha = 0
+        end
+    else
+        obj.alpha = 0
+    end
+    obj.draw()
+
+    -- カーソル
+    if obj.time < character_interval * character_count and positions[1] then
+        local cursor_height = positions[1][4]
+        local cursor_index = math.floor(obj.time / character_interval)
+        local cursor_position = positions[cursor_index + 1]
+        if cursor_position then
+            obj.load("figure", "四角形", cursor_color, cursor_height)
+            obj.effect("クリッピング", "上", cursor_height - cursor_thickness, "右", cursor_width_reduction)
+            obj.effect()
+            local cursor_x = cursor_position[1] + cursor_height / 2 - cursor_position[3] / 2
+            local cursor_y = cursor_position[2]
+            obj.draw(cursor_x, cursor_y)
         end
     end
-else
-    obj.alpha = 0
-end
-obj.draw()
-
---カーソル
-if obj.time < v * n then
-    size = tp[1][4]
-    obj.load("figure", "四角形", color, size)
-    obj.effect("クリッピング", "上", size - track1, "右", track0)
-    obj.effect()
-    cv = math.floor(obj.time / v)
-    x = tp[cv + 1][1] + size / 2 - tp[cv + 1][3] / 2
-    y = tp[cv + 1][2]
-    obj.draw(x, y)
 end
