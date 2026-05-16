1b015da8eb24c40ed56110a43209003d96df13b5
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-16 15:12:45 +0900

    feat: TAぼかし登場を移植

--- scripts/TA/TAぼかし登場.lua
+++ scripts/TA/TAぼかし登場.lua
@@ -3,70 +3,82 @@
 ---min=-5
 ---max=5
 ---step=0.01
-local ta = 0.3
+local duration = 0.3
 ---$track:間隔[s]
 ---min=0
 ---max=5
 ---step=0.01
-local tb = 0.3
+local interval = 0.3
 ---$track:範囲
 ---min=0
 ---max=300
 ---step=1
-local track2 = 10
----$track:登場順
----min=0
----max=5
----step=1
-local jun = 0
+local blur_range = 10
+---$select:登場順
+---順番=0
+---逆順=1
+---ランダム順=2
+---ランダム間隔=3
+---内側から=4
+---外側から=5
+local order = 0
 ---$check:フェード
-local fade = 0
+local fade = false
 
----$value:縦横比(-100〜100)
-local as = 0
+---$track:縦横比
+---min=-100
+---max=100
+---step=0.01
+local aspect = 0
 
----$value:拡大率
-local s = 150
+---$track:拡大率
+---min=0
+---max=1000
+---step=0.001
+local zoom_rate = 150
 
-if jun < 1 then
-    mode = obj.index --順番に登場
-elseif jun < 2 then
-    mode = obj.num - 1 - obj.index --後ろから登場
-elseif jun < 3 then
+local order_index
+if order == 0 then
+    order_index = obj.index -- 順番に登場
+elseif order == 1 then
+    order_index = obj.num - 1 - obj.index -- 後ろから登場
+elseif order == 2 then
     local indexes = {}
     for i = 0, obj.num - 1 do
         indexes[i + 1] = i
     end
     for i = 0, obj.num - 1 do
-        local dest = 0
-        dest = rand(0, obj.num - 1, -obj.num, i + 1)
+        local dest = rand(0, obj.num - 1, -obj.num, i + 1)
         local swap = indexes[i + 1]
         indexes[i + 1] = indexes[dest + 1]
         indexes[dest + 1] = swap
     end
-    mode = indexes[obj.index + 1] --ランダム順に登場
-elseif jun < 4 then
-    mode = rand(0, 100 * (obj.num - 1), obj.index, 0) / 100 --ランダム間隔に登場
-elseif jun < 5 then
-    mode = math.abs((obj.num - 1) / 2 - obj.index) --内側から登場
+    order_index = indexes[obj.index + 1] -- ランダム順に登場
+elseif order == 3 then
+    order_index = rand(0, 100 * (obj.num - 1), obj.index, 0) / 100 -- ランダム間隔に登場
+elseif order == 4 then
+    order_index = math.abs((obj.num - 1) / 2 - obj.index) -- 内側から登場
 else
-    mode = (obj.num - 1) / 2 - math.abs((obj.num - 1) / 2 - obj.index) --外側から登場
+    order_index = (obj.num - 1) / 2 - math.abs((obj.num - 1) / 2 - obj.index) -- 外側から登場
 end
 
-if ta < 0 then
-    i = (ta - obj.num * tb - obj.time + obj.totaltime + mode * tb) / ta
+local progress
+if duration < 0 then
+    progress = (duration - obj.num * interval - obj.time + obj.totaltime + order_index * interval) / duration
 else
-    i = (ta - obj.time + mode * tb) / ta
+    progress = (duration - obj.time + order_index * interval) / duration
 end
-if i > 0 then
-    if i > 1 then
+
+if progress > 0 then
+    if progress > 1 then
         obj.alpha = 0
-        i = 1
+        progress = 1
     end
-    i = i * i
-    obj.zoom = obj.zoom + i * (s - 100) / 100
-    if fade == 1 then
-        obj.alpha = obj.alpha * (1 - i)
+
+    progress = progress * progress
+    obj.zoom = obj.zoom + progress * (zoom_rate - 100) / 100
+    if fade then
+        obj.alpha = obj.alpha * (1 - progress)
     end
-    obj.effect("ぼかし", "範囲", track2 * i, "縦横比", as)
+    obj.effect("ぼかし", "範囲", blur_range * progress, "縦横比", aspect)
 end
