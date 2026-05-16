df6e4912abef0a670a63cfcf6dd1dbddbdcaca7b
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-17 08:22:13 +0900

    feat: TAランダム文字で登場を移植

--- scripts/TA/TAランダム文字で登場.lua
+++ scripts/TA/TAランダム文字で登場.lua
@@ -3,60 +3,70 @@
 ---min=-5
 ---max=5
 ---step=0.01
-local ta = 0.3
+local duration = 0.3
+
 ---$track:間隔[s]
 ---min=0
 ---max=5
 ---step=0.01
-local tb = 0.3
+local interval = 0.3
+
 ---$track:文字間隔
 ---min=-500
 ---max=500
-local w = 34
+local character_spacing = 34
+
 ---$track:拡大率
 ---min=0
 ---max=800
-local s = 100
----$value:切替フレーム
-local k = 3
+local zoom_rate = 100
+
+---$track:切替フレーム
+---min=1
+---max=60
+---step=1
+local switch_frames = 3
 
 ---$check:フェード
-local f = 0
+local fades = false
 
----$value:Y軸
-local y = 0
+---$track:Y軸
+---min=-1000
+---max=1000
+local y_distance = 0
 
 obj.effect()
 obj.ox = 0
 for i = 0, obj.num - 1 do
-    if ta == 0 then
+    local progress
+    if duration == 0 then
         return
-    elseif ta < 0 then
-        r = (ta - (obj.num - 1) * tb - obj.time + obj.totaltime + i * tb) / ta
+    elseif duration < 0 then
+        progress = (duration - (obj.num - 1) * interval - obj.time + obj.totaltime + i * interval) / duration
     else
-        r = (ta - obj.time + i * tb) / ta
+        progress = (duration - obj.time + i * interval) / duration
     end
 
-    a = 0
-    if r > 0 then
-        if r > 1 then
+    local alpha = 0
+    if progress > 0 then
+        if progress > 1 then
             obj.alpha = 0
-            r = 1
+            progress = 1
         end
-        r = r * r
-        if f == 1 then
-            obj.alpha = obj.alpha * (1 - r)
+        progress = progress * progress
+        if fades then
+            obj.alpha = obj.alpha * (1 - progress)
         end
-        obj.zoom = obj.zoom + r * (s - 100) / 100
-        obj.oy = obj.oy + y * r
-        ran = (math.floor(obj.frame / k) + i * 3) % obj.num
-        if obj.index == ran then
-            a = 1
+        obj.zoom = obj.zoom + progress * (zoom_rate - 100) / 100
+        obj.oy = obj.oy + y_distance * progress
+        local random_index = (math.floor(obj.frame / switch_frames) + i * 3) % obj.num
+        if obj.index == random_index then
+            alpha = 1
         end
     else
         if i == obj.index then
-            a = 1
+            alpha = 1
         end
     end
-    obj.draw(i * w, 0, 0, 1, a)
+    obj.draw(i * character_spacing, 0, 0, 1, alpha)
 end
