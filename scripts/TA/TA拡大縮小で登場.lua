c09de42c513576b5623b2a117a20dd4738747eb9
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-17 10:35:29 +0900

    feat: TA拡大縮小で登場を移植

--- scripts/TA/TA拡大縮小で登場.lua
+++ scripts/TA/TA拡大縮小で登場.lua
@@ -3,47 +3,73 @@
 ---min=-5
 ---max=5
 ---step=0.01
-local ta = 0.3
+local duration = 0.3
+
 ---$track:間隔[s]
 ---min=0
 ---max=5
 ---step=0.01
-local tb = 0.3
+local interval = 0.3
+
 ---$track:拡大率
 ---min=0
 ---max=1000
-local track2 = 300
----$track:登場順
----min=0
----max=5
----step=1
-local jun = 0
+---step=0.1
+local zoom_rate = 300
+
+---$select:登場順
+---順番=0
+---後ろから=1
+---ランダム順=2
+---ランダム間隔=3
+---内側から=4
+---外側から=5
+local order_mode = 0
+
 ---$check:フェード
-local fade = 0
+local fades = false
 
----$value:X距離
-local x = 0
+---$track:X距離
+---min=-2000
+---max=2000
+---step=0.1
+local distance_x = 0
 
----$value:Y距離
-local y = 0
+---$track:Y距離
+---min=-2000
+---max=2000
+---step=0.1
+local distance_y = 0
 
----$value:Z距離
-local z = 0
+---$track:Z距離
+---min=-2000
+---max=2000
+---step=0.1
+local distance_z = 0
 
----$value:X軸回転
-local rx = 0
+--trackgroup@distance_x,distance_y,distance_z:距離
 
----$value:Y軸回転
-local ry = 0
+---$track:X軸回転
+---min=-720
+---max=720
+---step=0.1
+local rotation_x = 0
 
----$value:Z軸回転
-local rz = 0
+---$track:Y軸回転
+---min=-720
+---max=720
+---step=0.1
+local rotation_y = 0
 
-if jun < 1 then
-    mode = obj.index --順番に登場
-elseif jun < 2 then
-    mode = obj.num - 1 - obj.index --後ろから登場
-elseif jun < 3 then
+---$track:Z軸回転
+---min=-720
+---max=720
+---step=0.1
+local rotation_z = 0
+
+--trackgroup@rotation_x,rotation_y,rotation_z:回転
+
+local function get_random_order_index()
     local indexes = {}
     for i = 0, obj.num - 1 do
         indexes[i + 1] = i
@@ -55,33 +81,49 @@ elseif jun < 3 then
         indexes[i + 1] = indexes[dest + 1]
         indexes[dest + 1] = swap
     end
-    mode = indexes[obj.index + 1] --ランダム順に登場
-elseif jun < 4 then
-    mode = rand(0, 100 * (obj.num - 1), obj.index, 0) / 100 --ランダム間隔に登場
-elseif jun < 5 then
-    mode = math.abs((obj.num - 1) / 2 - obj.index) --内側から登場
-else
-    mode = (obj.num - 1) / 2 - math.abs((obj.num - 1) / 2 - obj.index) --外側から登場
+    return indexes[obj.index + 1]
 end
 
-if ta < 0 then
-    i = (ta - obj.num * tb - obj.time + obj.totaltime + mode * tb) / ta
-else
-    i = (ta - obj.time + mode * tb) / ta
-end
-if i > 0 then
-    if i > 1 then
-        obj.alpha = 0
-        i = 1
+local function get_order_index()
+    if order_mode == 0 then
+        return obj.index
+    elseif order_mode == 1 then
+        return obj.num - 1 - obj.index
+    elseif order_mode == 2 then
+        return get_random_order_index()
+    elseif order_mode == 3 then
+        return rand(0, 100 * (obj.num - 1), obj.index, 0) / 100
+    elseif order_mode == 4 then
+        return math.abs((obj.num - 1) / 2 - obj.index)
     end
-    obj.zoom = obj.zoom + 4 * (i - i * i * i) * (track2 - 100) / 100
-    obj.ox = obj.ox + x * i
-    obj.oy = obj.oy + y * i
-    obj.oz = obj.oz + z * i
-    obj.rx = obj.rx + rx * i
-    obj.ry = obj.ry + ry * i
-    obj.rz = obj.rz + rz * i
-    if fade == 1 then
-        obj.alpha = obj.alpha * (1 - i)
+    return (obj.num - 1) / 2 - math.abs((obj.num - 1) / 2 - obj.index)
+end
+
+if duration == 0 then
+    return
+end
+
+local order_index = get_order_index()
+local progress
+if duration < 0 then
+    progress = (duration - obj.num * interval - obj.time + obj.totaltime + order_index * interval) / duration
+else
+    progress = (duration - obj.time + order_index * interval) / duration
+end
+
+if progress > 0 then
+    if progress > 1 then
+        obj.alpha = 0
+        progress = 1
+    end
+    obj.zoom = obj.zoom + 4 * (progress - progress * progress * progress) * (zoom_rate - 100) / 100
+    obj.ox = obj.ox + distance_x * progress
+    obj.oy = obj.oy + distance_y * progress
+    obj.oz = obj.oz + distance_z * progress
+    obj.rx = obj.rx + rotation_x * progress
+    obj.ry = obj.ry + rotation_y * progress
+    obj.rz = obj.rz + rotation_z * progress
+    if fades then
+        obj.alpha = obj.alpha * (1 - progress)
     end
 end
