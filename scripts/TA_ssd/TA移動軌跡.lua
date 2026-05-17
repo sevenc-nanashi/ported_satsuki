6313462083cfe25c04a345ea46908e445519db57
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-17 11:27:11 +0900

    feat: TA移動軌跡を移植

--- scripts/TA_ssd/TA移動軌跡.lua
+++ scripts/TA_ssd/TA移動軌跡.lua
@@ -3,67 +3,73 @@
 ---min=0
 ---max=100
 ---step=0.01
-local track0 = 50
+local position_rate = 50
+
 ---$track:字間
 ---min=0
 ---max=512
-local track1 = 34
----$track:始終透明
----min=0
----max=1
----step=1
-local track2 = 0
+---step=0.1
+local spacing = 34
+
+---$check:始終透明
+local hides_ends = false
+
 ---$check:時間基準
-local __rename_me_check0 = false
+local uses_time_basis = false
 
 obj.effect()
 
-if __rename_me_check0 then
-    otm_j = obj.time / obj.totaltime
+local track_position_rate
+if uses_time_basis then
+    track_position_rate = obj.time / obj.totaltime
 else
-    otm_j = track0 / 100
+    track_position_rate = position_rate / 100
 end
 
-t = otm_j * (obj.totaltime + (obj.num - 1) * track1 / 100) - obj.index * track1 / 100
+local time = track_position_rate * (obj.totaltime + (obj.num - 1) * spacing / 100) - obj.index * spacing / 100
 
-if t < 0 then
-    t = 0
-    obj.alpha = 1 - track2
-elseif t > obj.totaltime then
-    t = obj.totaltime
-    obj.alpha = 1 - track2
+if time < 0 then
+    time = 0
+    obj.alpha = hides_ends and 0 or 1
+elseif time > obj.totaltime then
+    time = obj.totaltime
+    obj.alpha = hides_ends and 0 or 1
 end
 
 obj.ox = 0
 obj.oy = 0
 
-base_x = obj.getvalue("x")
-base_y = obj.getvalue("y")
+local base_x = obj.getvalue("x")
+local base_y = obj.getvalue("y")
 
 obj.ox = obj.ox - base_x
 obj.oy = obj.oy - base_y
 
-x0 = obj.getvalue("x", t)
-y0 = obj.getvalue("y", t)
-x = x0 - obj.getvalue("x", t + 1 / obj.framerate)
-y = y0 - obj.getvalue("y", t + 1 / obj.framerate)
-r = math.atan2(x, y) - math.pi / 2
-sx = math.sin(r) * obj.w / 2
-cx = math.cos(r) * obj.w / 2
-sy = math.sin(r) * obj.h / 2
-cy = math.cos(r) * obj.h / 2
+local x0 = obj.getvalue("x", time)
+local y0 = obj.getvalue("y", time)
+local next_x = obj.getvalue("x", time + 1 / obj.framerate)
+local next_y = obj.getvalue("y", time + 1 / obj.framerate)
+local direction_x = x0 - next_x
+local direction_y = y0 - next_y
+local rotation = math.atan2(direction_x, direction_y) - math.pi / 2
+local sin_rotation = math.sin(rotation)
+local cos_rotation = math.cos(rotation)
+local half_width_sin = sin_rotation * obj.w / 2
+local half_width_cos = cos_rotation * obj.w / 2
+local half_height_sin = sin_rotation * obj.h / 2
+local half_height_cos = cos_rotation * obj.h / 2
 obj.drawpoly(
-    x0 - cx - sy,
-    y0 + sx - cy,
+    x0 - half_width_cos - half_height_sin,
+    y0 + half_width_sin - half_height_cos,
     0,
-    x0 + cx - sy,
-    y0 - sx - cy,
+    x0 + half_width_cos - half_height_sin,
+    y0 - half_width_sin - half_height_cos,
     0,
-    x0 + cx + sy,
-    y0 - sx + cy,
+    x0 + half_width_cos + half_height_sin,
+    y0 - half_width_sin + half_height_cos,
     0,
-    x0 - cx + sy,
-    y0 + sx + cy,
+    x0 - half_width_cos + half_height_sin,
+    y0 + half_width_sin + half_height_cos,
     0,
     0,
     0,
