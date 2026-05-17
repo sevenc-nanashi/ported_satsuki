007fd9c06fe2bfbea1d9bbaae05703ab6a53088b
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-17 15:26:56 +0900

    feat: 加減速Sを移植

--- scripts/ANM_ssd/加減速S.lua
+++ scripts/ANM_ssd/加減速S.lua
@@ -1,13 +1,14 @@
 --label:${ROOT_CATEGORY}
---param:2
+--param:べき数,2
 local index, ratio = math.modf(obj.getpoint("index"))
-local st = obj.getpoint(index)
-local ed = obj.getpoint(index + 1)
+local val_start = obj.getpoint(index)
+local val_end = obj.getpoint(index + 1)
 
+local value
 if ratio <= 0.5 then
-    kagen = st + (ed - st) * (2 * ratio) ^ obj.getpoint("param") / 2
+    value = val_start + (val_end - val_start) * (2 * ratio) ^ obj.getpoint("param") / 2
 else
-    kagen = st + (ed - st) * ((1 - (1 - (ratio - 0.5) * 2) ^ obj.getpoint("param")) / 2 + 0.5)
+    value = val_start + (val_end - val_start) * ((1 - (1 - (ratio - 0.5) * 2) ^ obj.getpoint("param")) / 2 + 0.5)
 end
 
-return kagen
+return value
