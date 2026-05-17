b61c9b8550e59608f94ab66179f1f5c2c48287fc
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-17 09:03:50 +0900

    feat: TA位置調整で移動を移植

--- scripts/TA_ssd/TA位置調整で移動.lua
+++ scripts/TA_ssd/TA位置調整で移動.lua
@@ -3,50 +3,59 @@
 ---min=-5
 ---max=5
 ---step=0.01
-local ta = 0.3
+local duration = 0.3
+
 ---$track:間隔[s]
 ---min=0
 ---max=5
 ---step=0.01
-local tb = 0.3
+local interval = 0.3
+
 ---$track:開始[s]
 ---min=0
 ---max=100
 ---step=0.01
-local st = 0
----$track:登場順
----min=0
+local start_time = 0
+
+---$select:登場順
+---順番=0
+---後ろから=1
+---ランダム順=2
+---ランダム間隔=3
+---内側から=4
+---外側から=5
+local order_mode = 0
+
+---$track:加減速
+---min=1
 ---max=5
 ---step=1
-local jun = 0
----$value:加減速[1-5]
-local beki = 2
+local easing_power = 2
 
----$value:減
-local adc = 0
+---$select:減/加/S
+---減速=0
+---加速=1
+---減速加速=2
+local easing_mode = 0
 
 ---$check:タイプ
-local type = 0
+local overall_appearance = false
 
 ---$value:座標
-local pos = {}
+local positions = {}
 
 obj.effect()
 
-obj.setanchor("pos", obj.num, "line")
+obj.setanchor("positions", obj.num, "line")
 
 if obj.index == 0 then
-    if pp == null then
-        pp = {}
+    if S_position_move_states == nil then
+        S_position_move_states = {}
     end
 end
-pp[obj.index + 1] = { obj.ox, obj.oy }
+S_position_move_states[obj.index + 1] = { obj.ox, obj.oy }
 
-if jun < 1 then
-    mode = obj.index --順番に登場
-elseif jun < 2 then
-    mode = obj.num - 1 - obj.index --後ろから登場
-elseif jun < 3 then
+local function get_random_order_index()
     local indexes = {}
     for i = 0, obj.num - 1 do
         indexes[i + 1] = i
@@ -58,49 +67,76 @@ elseif jun < 3 then
         indexes[i + 1] = indexes[dest + 1]
         indexes[dest + 1] = swap
     end
-    mode = indexes[obj.index + 1] --ランダム順に登場
-elseif jun < 4 then
-    mode = rand(0, 100 * (obj.num - 1), obj.index, 0) / 100 --ランダム間隔に登場
-elseif jun < 5 then
-    mode = math.abs((obj.num - 1) / 2 - obj.index) --内側から登場
-else
-    mode = (obj.num - 1) / 2 - math.abs((obj.num - 1) / 2 - obj.index) --外側から登場
+    return indexes[obj.index + 1]
 end
 
-if type < 1 then
-else
-    ta = ta * (1 - mode / obj.num)
-    tb = tb / obj.num
-end
-
-if ta < 0 then
-    r = (ta - obj.num * tb - obj.time - st + obj.totaltime + mode * tb) / ta
-else
-    r = (ta - obj.time + st + mode * tb) / ta
-end
-if r > 0 then
-    if r > 1 then
-        r = 1
-    end
-    if adc < 1 then
-        r = r ^ beki
-    elseif adc < 2 then
-        r = 1 - (1 - r) ^ beki
+local function get_order_index()
+    if order_mode == 0 then
+        return obj.index
+    elseif order_mode == 1 then
+        return obj.num - 1 - obj.index
+    elseif order_mode == 2 then
+        return get_random_order_index()
+    elseif order_mode == 3 then
+        return rand(0, 100 * (obj.num - 1), obj.index, 0) / 100
+    elseif order_mode == 4 then
+        return math.abs((obj.num - 1) / 2 - obj.index)
     else
-        if r <= 0.5 then
-            r = (2 * r) ^ beki / 2
-        else
-            r = (1 - (1 - (r - 0.5) * 2) ^ beki) / 2 + 0.5
-        end
+        return (obj.num - 1) / 2 - math.abs((obj.num - 1) / 2 - obj.index)
     end
-    for i = 0, obj.num - 1 do
-        x = pos[i * 2 + 1] - pp[i + 1][1]
-        y = pos[i * 2 + 2] - pp[i + 1][2]
-        if i == obj.index then
-            alp = 1
-        else
-            alp = 0
-        end
-        obj.draw(x * r, y * r, 0, 1, alp)
+end
+
+local function apply_easing(progress)
+    if easing_mode == 0 then
+        return progress ^ easing_power
+    elseif easing_mode == 1 then
+        return 1 - (1 - progress) ^ easing_power
     end
+
+    if progress <= 0.5 then
+        return (2 * progress) ^ easing_power / 2
+    end
+    return (1 - (1 - (progress - 0.5) * 2) ^ easing_power) / 2 + 0.5
+end
+
+local order_index = get_order_index()
+local adjusted_duration = duration
+local adjusted_interval = interval
+if overall_appearance then
+    adjusted_duration = adjusted_duration * (1 - order_index / obj.num)
+    adjusted_interval = adjusted_interval / obj.num
+end
+
+if adjusted_duration == 0 then
+    return
+end
+
+local progress
+if adjusted_duration < 0 then
+    progress = (
+        adjusted_duration
+        - obj.num * adjusted_interval
+        - obj.time
+        - start_time
+        + obj.totaltime
+        + order_index * adjusted_interval
+    ) / adjusted_duration
+else
+    progress = (adjusted_duration - obj.time + start_time + order_index * adjusted_interval) / adjusted_duration
+end
+
+if progress <= 0 then
+    return
+end
+
+progress = apply_easing(math.min(progress, 1))
+for i = 0, obj.num - 1 do
+    local position_state = S_position_move_states[i + 1]
+    local x = positions[i * 2 + 1] - position_state[1]
+    local y = positions[i * 2 + 2] - position_state[2]
+    local alpha = 0
+    if i == obj.index then
+        alpha = 1
+    end
+    obj.draw(x * progress, y * progress, 0, 1, alpha)
 end
