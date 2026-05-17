05a2a367d19d8384b526125a2ecbd6c12e1dbe53
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-17 15:12:35 +0900

    feat: 加減速移動を移植

--- scripts/削除済み/加減速移動β.lua
+++ scripts/削除済み/加減速移動β.lua
@@ -3,100 +3,96 @@
 ---min=1
 ---max=5
 ---step=1
-local beki = 2
----$track:減/加/S
----min=0
----max=2
----step=1
-local track1 = 0
----$track:非表示
----min=0
----max=1
----step=1
-local track2 = 0
+local easing_power = 2
+---$select:モード
+---減速=0
+---加速=1
+---S字=2
+local mode = 0
+---$check:非表示
+local hidden = false
+
 ---$check:X軸
-local xx = 1
+local include_x = 1
 
 ---$check:Y軸
-local yy = 1
+local include_y = 1
 
 ---$check:Z軸
-local zz = 1
+local include_z = 1
 
 ---$check:拡大率
-local ss = 1
+local include_scale = 1
 
 ---$color:色
 local color = 0xff0000
 
 --beki=math.floor(beki)
-mode = math.floor(track1)
+mode = math.floor(mode)
 
-t = 1 - obj.time / obj.totaltime
+local progress = 1 - obj.time / obj.totaltime
 
-if t > 0 then
-    if t > 1 then
-        t = 1
+if progress > 0 then
+    if progress > 1 then
+        progress = 1
     end
     if mode < 1 then
-        t = t ^ beki
+        progress = progress ^ easing_power
     elseif mode < 2 then
-        t = 1 - (1 - t) ^ beki
+        progress = 1 - (1 - progress) ^ easing_power
     else
-        if t <= 0.5 then
-            t = (2 * t) ^ beki / 2
+        if progress <= 0.5 then
+            progress = (2 * progress) ^ easing_power / 2
         else
-            t = (1 - (1 - (t - 0.5) * 2) ^ beki) / 2 + 0.5
+            progress = (1 - (1 - (progress - 0.5) * 2) ^ easing_power) / 2 + 0.5
         end
     end
 
-    dx = (obj.getvalue("x", obj.totaltime) - obj.getvalue("x", 0)) * (1 - t)
-    dy = (obj.getvalue("y", obj.totaltime) - obj.getvalue("y", 0)) * (1 - t)
-    dz = (obj.getvalue("z", obj.totaltime) - obj.getvalue("z", 0)) * (1 - t)
-    ds = (obj.getvalue("zoom", obj.totaltime) - obj.getvalue("zoom", 0)) * (1 - t)
+    local dx = (obj.getvalue("x", obj.totaltime) - obj.getvalue("x", 0)) * (1 - progress)
+    local dy = (obj.getvalue("y", obj.totaltime) - obj.getvalue("y", 0)) * (1 - progress)
+    local dz = (obj.getvalue("z", obj.totaltime) - obj.getvalue("z", 0)) * (1 - progress)
+    local ds = (obj.getvalue("zoom", obj.totaltime) - obj.getvalue("zoom", 0)) * (1 - progress)
 
-    obj.ox = obj.ox + (obj.getvalue("x", 0) + dx - obj.getvalue("x")) * xx
-    obj.oy = obj.oy + (obj.getvalue("y", 0) + dy - obj.getvalue("y")) * yy
-    obj.oz = obj.oz + (obj.getvalue("z", 0) + dz - obj.getvalue("z")) * zz
-    obj.zoom = obj.zoom + (obj.getvalue("zoom", 0) + ds - obj.getvalue("zoom")) * ss / 100
+    obj.ox = obj.ox + (obj.getvalue("x", 0) + dx - obj.getvalue("x")) * include_x
+    obj.oy = obj.oy + (obj.getvalue("y", 0) + dy - obj.getvalue("y")) * include_y
+    obj.oz = obj.oz + (obj.getvalue("z", 0) + dz - obj.getvalue("z")) * include_z
+    obj.zoom = obj.zoom + (obj.getvalue("zoom", 0) + ds - obj.getvalue("zoom")) * include_scale / 100
 end
 obj.draw()
 
 --補助線
-a = 1 / 2
-if track2 == 1 then
-    a = 0
-end
+local alpha = 1 / 2
+if not hidden then
+    local h = obj.screen_h / 3
+    local zoom = 100 / obj.getvalue("zoom")
 
-h = obj.screen_h / 3
-zoom = 100 / obj.getvalue("zoom")
+    obj.setoption("billboard", 3)
+    obj.load("figure", "円", color, 50)
+    obj.draw(-h - obj.getvalue("x"), h - obj.getvalue("y"), 0, zoom, alpha)
+    obj.setoption("billboard", 3)
+    obj.load("figure", "円", color, 50)
+    obj.draw(h - obj.getvalue("x"), -h - obj.getvalue("y"), 0, zoom, alpha)
 
-obj.setoption("billboard", 3)
-obj.load("figure", "円", color, 50)
-obj.draw(-h - obj.getvalue("x"), h - obj.getvalue("y"), 0, zoom, a)
-obj.setoption("billboard", 3)
-obj.load("figure", "円", color, 50)
-obj.draw(h - obj.getvalue("x"), -h - obj.getvalue("y"), 0, zoom, a)
+    obj.setoption("billboard", 3)
+    obj.load("figure", "円", color, 10)
+    local num_points = 20
+    for i = 0, num_points do
+        local point_rate = i / num_points
+        local point_progress = 1 - point_rate
 
-obj.setoption("billboard", 3)
-obj.load("figure", "円", color, 10)
-n = 20
-for i = 0, n do
-    kn = i / n
-    kt = 1 - kn
-
-    if mode < 1 then
-        kt = kt ^ beki
-    elseif mode < 2 then
-        kt = 1 - (1 - kt) ^ beki
-    else
-        if kt <= 0.5 then
-            kt = (2 * kt) ^ beki / 2
+        if mode < 1 then
+            point_progress = point_progress ^ easing_power
+        elseif mode < 2 then
+            point_progress = 1 - (1 - point_progress) ^ easing_power
         else
-            kt = (1 - (1 - (kt - 0.5) * 2) ^ beki) / 2 + 0.5
+            if point_progress <= 0.5 then
+                point_progress = (2 * point_progress) ^ easing_power / 2
+            else
+                point_progress = (1 - (1 - (point_progress - 0.5) * 2) ^ easing_power) / 2 + 0.5
+            end
         end
+        local x = point_rate * h * 2 - h - obj.getvalue("x")
+        local y = -h + point_progress * h * 2 - obj.getvalue("y")
+        obj.draw(x, y, 0, zoom, alpha)
     end
-    x = kn * h * 2 - h - obj.getvalue("x")
-    y = -h + kt * h * 2 - obj.getvalue("y")
-    obj.draw(x, y, 0, zoom, a)
 end
