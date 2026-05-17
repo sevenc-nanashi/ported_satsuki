a77a0be1440e6b4502f4f2e247f4f2b6b5c04881
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-17 10:54:48 +0900

    feat: TA数珠つなぎを移植

--- scripts/TA_ssd/TA数珠つなぎ.lua
+++ scripts/TA_ssd/TA数珠つなぎ.lua
@@ -2,61 +2,79 @@
 ---$track:角度範囲
 ---min=-360
 ---max=360
-local track0 = 60
+---step=0.1
+local angle_range = 60
+
 ---$track:方向
 ---min=-360
 ---max=360
-local off_r = 0
+---step=0.1
+local direction_angle = 0
+
 ---$track:字間
 ---min=-800
 ---max=800
-local track2 = 100
+---step=0.1
+local spacing_rate = 100
+
 ---$track:拡大
 ---min=1
 ---max=100
-local track3 = 100
+---step=0.1
+local min_zoom_rate = 100
+
 ---$check:回転あり
-local rc = 0
+local rotates = false
 
----$value:ランダム種
-local seed = 1
+---$track:乱数
+---min=0
+---max=1000
+---step=1
+local random_seed = 1
 
-r = track0 / 2
+local half_angle_range = angle_range / 2
 
 --字間の配列
 if obj.index == 0 then
-    if tj == null then
-        tj = {}
-    end
+    S_spacing_positions = {}
 end
-tj[obj.index + 1] = { obj.ox, obj.oy }
+S_spacing_positions = S_spacing_positions or {}
+S_spacing_positions[obj.index + 1] = { obj.ox, obj.oy }
 
 --ランダム拡大の配列
-rzoom = {}
-idz = 0
+local random_zoom_rates = {}
 for i = 1, obj.num + 1 do
-    zz = obj.rand(track3, 100, -11 - seed, i) / 100
-    idz = idz + 1
-    rzoom[idz] = zz
+    random_zoom_rates[i] = obj.rand(min_zoom_rate, 100, -11 - random_seed, i) / 100
 end
 
 --メイン
 for i = 0, obj.num - 1 do
     if obj.index == i then
-        xx = 0
-        yy = 0
+        local x = 0
+        local y = 0
         if i > 0 then
             for j = 1, i do
-                l = track2 / 100 * (tj[j + 1][1] - tj[j][1]) * (rzoom[j] + rzoom[j + 1]) / 2
-                xx = xx + l * math.cos(math.rad(off_r + obj.rand(-r, r, -1 - seed, j)))
-                yy = yy + l * math.sin(math.rad(off_r + obj.rand(-r, r, -1 - seed, j)))
+                local distance = spacing_rate
+                    / 100
+                    * (S_spacing_positions[j + 1][1] - S_spacing_positions[j][1])
+                    * (random_zoom_rates[j] + random_zoom_rates[j + 1])
+                    / 2
+                local angle = direction_angle + obj.rand(-half_angle_range, half_angle_range, -1 - random_seed, j)
+                x = x + distance * math.cos(math.rad(angle))
+                y = y + distance * math.sin(math.rad(angle))
             end
         end
-        obj.ox = xx
-        obj.oy = yy
-        if rc == 1 then
-            obj.rz = obj.rz + off_r + (obj.rand(-r, r, -1, i) + obj.rand(-r, r, -1 - seed, i + 1)) / 2
+        obj.ox = x
+        obj.oy = y
+        if rotates then
+            obj.rz = obj.rz
+                + direction_angle
+                + (
+                        obj.rand(-half_angle_range, half_angle_range, -1, i)
+                        + obj.rand(-half_angle_range, half_angle_range, -1 - random_seed, i + 1)
+                    )
+                    / 2
         end
-        obj.zoom = obj.zoom * rzoom[i + 1]
+        obj.zoom = obj.zoom * random_zoom_rates[i + 1]
     end
 end
