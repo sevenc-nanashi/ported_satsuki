5ec46d0a8294710e559d4975e46557bc5fad28bf
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-17 10:29:42 +0900

    feat: TA字間移動で登場を移植

--- scripts/TA/TA字間移動で登場.lua
+++ scripts/TA/TA字間移動で登場.lua
@@ -3,42 +3,55 @@
 ---min=-10
 ---max=10
 ---step=0.01
-local t = 3
----$track:字間率
+local duration = 3
+
+---$track:字間率[%]
 ---min=-1000
 ---max=1000
-local track1 = 300
+---step=0.1
+local spacing_rate = 300
+
 ---$track:中心座標
 ---min=-1000
 ---max=1000
-local track2 = 0
+---step=0.1
+local center_position = 0
+
 ---$track:加減速
 ---min=1
 ---max=5
 ---step=1
-local track3 = 2
+local easing_power = 2
+
 ---$check:縦書き
-local tate = 0
+local vertical = false
 
 ---$check:フェード
-local fade = 0
+local fades = false
 
-if t == 0 then
+if duration == 0 then
     return
-elseif t < 0 then
-    i = (t - obj.time + obj.totaltime) / t
-else
-    i = (t - obj.time) / t
 end
-if i > 0 then
-    if i > 1 then
-        i = 1
-    end
-    i = i ^ track3
-    if tate == 1 then
-        obj.oy = track2 + (obj.oy - track2) * (1 + track1 / 100 * i)
-    else
-        obj.ox = track2 + (obj.ox - track2) * (1 + track1 / 100 * i)
-    end
-    obj.alpha = obj.alpha * (1 - i * fade)
+
+local progress
+if duration < 0 then
+    progress = (duration - obj.time + obj.totaltime) / duration
+else
+    progress = (duration - obj.time) / duration
+end
+
+if progress <= 0 then
+    return
+end
+
+progress = math.min(progress, 1) ^ easing_power
+local spacing_scale = 1 + spacing_rate / 100 * progress
+if vertical then
+    obj.oy = center_position + (obj.oy - center_position) * spacing_scale
+else
+    obj.ox = center_position + (obj.ox - center_position) * spacing_scale
+end
+
+if fades then
+    obj.alpha = obj.alpha * (1 - progress)
 end
