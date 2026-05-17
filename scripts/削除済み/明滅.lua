771f2c85599a2dbd98ed931d293a5ad6eb21a680
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-17 15:13:39 +0900

    feat: 明滅を移植

--- scripts/削除済み/明滅.lua
+++ scripts/削除済み/明滅.lua
@@ -2,23 +2,24 @@
 ---$track:間隔[ms]
 ---min=0
 ---max=5000
-local track0 = 1000
----$track:モード
----min=0
----max=3
----step=1
+local interval_ms = 1000
+---$select:モード
+---ノーマル=0
+---フェードアウト=1
+---フェードイン=2
+---sin波=3
 local mode = 0
-t = track0 / 1000
+local interval_s = interval_ms / 1000
 if mode < 1 then --ノーマル
-    if obj.time % t < t / 2 then
+    if obj.time % interval_s < interval_s / 2 then
         obj.alpha = 1.0
     else
         obj.alpha = 0.0
     end
 elseif mode < 2 then --フェードアウト
-    obj.alpha = 1.0 - (obj.time % t) / t
+    obj.alpha = 1.0 - (obj.time % interval_s) / interval_s
 elseif mode < 3 then --フェードイン
-    obj.alpha = (obj.time % t) / t
+    obj.alpha = (obj.time % interval_s) / interval_s
 else --フェードイン
-    obj.alpha = math.sin(2 * math.pi * obj.time * (1 / t)) * 0.5 + 0.5
+    obj.alpha = math.sin(2 * math.pi * obj.time * (1 / interval_s)) * 0.5 + 0.5
 end
