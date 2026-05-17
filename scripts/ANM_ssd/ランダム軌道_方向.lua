b0f8e15664d4ab9653a79fa1251c6f4e71f3ef28
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-17 14:09:08 +0900

    feat: ランダム軌道_方向を移植

--- scripts/ANM_ssd/ランダム軌道_方向.lua
+++ scripts/ANM_ssd/ランダム軌道_方向.lua
@@ -2,41 +2,55 @@
 ---$track:振幅
 ---min=0
 ---max=500
-local track0 = 200
+---step=0.1
+local amplitude = 200
+
 ---$track:間隔
 ---min=1
 ---max=500
-local track1 = 100
+---step=0.1
+local interval = 100
+
 ---$track:個数
 ---min=1
 ---max=100
 ---step=1
-local n = 3
----$value:ランダムサイズ[-%]
-local rs = 0
+local count = 3
 
----$value:ランダム透明度[-%]
-local ralp = 0
+---$track:ランダムサイズ[-%]
+---min=0
+---max=100
+---step=0.1
+local random_zoom_reduction = 0
 
----$value:差分
-local sbn = 7.7
+---$track:ランダム透明度[-%]
+---min=0
+---max=100
+---step=0.1
+local random_alpha_reduction = 0
 
-for i = 0, n - 1 do
-    t = (obj.time + i * sbn) * 100 / track1
-    p = math.floor(t)
-    t = t - p
-    x0 = obj.rand(-1000, 1000, 0 + i, p + 0) / 1000 * track0
-    x1 = obj.rand(-1000, 1000, 0 + i, p + 1) / 1000 * track0
-    x2 = obj.rand(-1000, 1000, 0 + i, p + 2) / 1000 * track0
-    x3 = obj.rand(-1000, 1000, 0 + i, p + 3) / 1000 * track0
-    y0 = obj.rand(-1000, 1000, 1 + i, p + 0) / 1000 * track0
-    y1 = obj.rand(-1000, 1000, 1 + i, p + 1) / 1000 * track0
-    y2 = obj.rand(-1000, 1000, 1 + i, p + 2) / 1000 * track0
-    y3 = obj.rand(-1000, 1000, 1 + i, p + 3) / 1000 * track0
-    x, y = obj.interpolation(t, x0, y0, x1, y1, x2, y2, x3, y3)
-    xx, yy = obj.interpolation(t + 1 / obj.framerate, x0, y0, x1, y1, x2, y2, x3, y3)
-    s = 1 - obj.rand(0, 1000, 10 + i, 10) / 1000 * rs / 100
-    alp = 1 - obj.rand(0, 1000, 11 + i, 11) / 1000 * ralp / 100
-    r = math.deg(math.atan2(yy - y, xx - x)) + 90
-    obj.draw(x, y, 0, s, alp, 0, 0, r)
+---$track:差分
+---min=-100
+---max=100
+---step=0.1
+local time_offset = 7.7
+
+for i = 0, count - 1 do
+    local time = (obj.time + i * time_offset) * 100 / interval
+    local point_index = math.floor(time)
+    local progress = time - point_index
+    local x0 = obj.rand(-1000, 1000, i, point_index) / 1000 * amplitude
+    local x1 = obj.rand(-1000, 1000, i, point_index + 1) / 1000 * amplitude
+    local x2 = obj.rand(-1000, 1000, i, point_index + 2) / 1000 * amplitude
+    local x3 = obj.rand(-1000, 1000, i, point_index + 3) / 1000 * amplitude
+    local y0 = obj.rand(-1000, 1000, 1 + i, point_index) / 1000 * amplitude
+    local y1 = obj.rand(-1000, 1000, 1 + i, point_index + 1) / 1000 * amplitude
+    local y2 = obj.rand(-1000, 1000, 1 + i, point_index + 2) / 1000 * amplitude
+    local y3 = obj.rand(-1000, 1000, 1 + i, point_index + 3) / 1000 * amplitude
+    local x, y = obj.interpolation(progress, x0, y0, x1, y1, x2, y2, x3, y3)
+    local next_x, next_y = obj.interpolation(progress + 1 / obj.framerate, x0, y0, x1, y1, x2, y2, x3, y3)
+    local zoom = 1 - obj.rand(0, 1000, 10 + i, 10) / 1000 * random_zoom_reduction / 100
+    local alpha = 1 - obj.rand(0, 1000, 11 + i, 11) / 1000 * random_alpha_reduction / 100
+    local rotation = math.deg(math.atan2(next_y - y, next_x - x)) + 90
+    obj.draw(x, y, 0, zoom, alpha, 0, 0, rotation)
 end
