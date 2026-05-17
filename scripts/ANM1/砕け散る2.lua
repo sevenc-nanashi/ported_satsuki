8c6fb391990013dc6ee6dfc0763226212d20f49f
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-17 14:58:15 +0900

    feat: 砕け散る2を移植

--- scripts/ANM1/砕け散る2.lua
+++ scripts/ANM1/砕け散る2.lua
@@ -3,173 +3,209 @@
 ---min=0
 ---max=50
 ---step=0.01
-local track0 = 0
+local elapsed_time = 0
+
 ---$track:中心X
 ---min=-1000
 ---max=1000
-local ax = 0
+---step=0.1
+local center_x = 0
+
 ---$track:中心Y
 ---min=-1000
 ---max=1000
-local ay = 0
----$track:中心z
+---step=0.1
+local center_y = 0
+
+---$track:中心Z
 ---min=-1000
 ---max=1000
-local az = 0
----$value:破片サイズ
-local size = 50
+---step=0.1
+local center_z = 0
 
----$value:速度
+--trackgroup@center_x,center_y,center_z:中心
+
+---$track:破片サイズ
+---min=10
+---max=500
+---step=0.1
+local fragment_size = 50
+
+---$track:速度
+---min=0
+---max=1000
+---step=0.1
 local speed = 100
 
----$value:重力
-local grav = 100
+---$track:重力
+---min=-1000
+---max=1000
+---step=0.1
+local gravity = 100
 
----$value:時間差
+---$track:時間差
+---min=0
+---max=1000
+---step=0.1
 local delay = 100
 
----$value:距離影響
-local impact = 100
+---$track:距離影響
+---min=0
+---max=1000
+---step=0.1
+local distance_impact = 100
 
----$value:ランダム回転
-local spin = 100
+---$track:ランダム回転
+---min=0
+---max=1000
+---step=0.1
+local random_spin = 100
 
----$value:ランダム方向
-local diff = 100
+---$track:ランダム方向
+---min=0
+---max=1000
+---step=0.1
+local random_direction = 100
 
----$value:再生速度
-local step = 1.0
+---$track:再生速度
+---min=0
+---max=10
+---step=0.01
+local playback_speed = 1.0
 
 local px = {}
 local py = {}
 local pz = {}
 local pu = {}
 local pv = {}
+local vertices = {}
 
-tm = track0 / 10 * step
+local time = elapsed_time / 10 * playback_speed
 obj.effect()
-grav = grav * 6
-delay = delay * 0.002
-impact = impact * 0.1
-spin = math.floor(spin * 10)
-diff = diff / 80
-if size < 10 then
-    size = 10
+
+local gravity_acceleration = gravity * 6
+local delay_scale = delay * 0.002
+local impact_scale = distance_impact * 0.1
+local spin_scale = math.floor(random_spin * 10)
+local direction_scale = random_direction / 80
+local width = obj.w
+local height = obj.h
+local speed_scale = speed * 0.01 * math.sqrt(impact_scale)
+local fragment_columns = math.floor(width / fragment_size)
+local fragment_rows = math.floor(height / fragment_size)
+local diagonal_length = math.sqrt(width * width + height * height)
+
+if fragment_columns < 2 then
+    fragment_columns = 2
+elseif fragment_columns > width then
+    fragment_columns = width
 end
-xl = obj.w
-yl = obj.h
-sp = speed * 0.01 * math.sqrt(impact)
-w = math.floor(xl / size)
-h = math.floor(yl / size)
-l = math.sqrt(xl * xl + yl * yl)
-if w < 2 then
-    w = 2
-elseif w > xl then
-    w = xl
+if fragment_rows < 2 then
+    fragment_rows = 2
+elseif fragment_rows > height then
+    fragment_rows = height
 end
-if h < 2 then
-    h = 2
-elseif h > yl then
-    h = yl
-end
-pw = xl / w / 2.3
-ph = yl / h / 2.3
-for y = 0, h - 1 do
-    for x = 0, w - 1 do
+
+local random_width = width / fragment_columns / 2.3
+local random_height = height / fragment_rows / 2.3
+for y = 0, fragment_rows - 1 do
+    for x = 0, fragment_columns - 1 do
         -- 各頂点を計算
-        pu[0] = xl * x / w
-        pu[1] = xl * (x + 1) / w
+        pu[0] = width * x / fragment_columns
+        pu[1] = width * (x + 1) / fragment_columns
         pu[2] = pu[1]
         pu[3] = pu[0]
-        pv[0] = yl * y / h
+        pv[0] = height * y / fragment_rows
         pv[1] = pv[0]
-        pv[2] = yl * (y + 1) / h
+        pv[2] = height * (y + 1) / fragment_rows
         pv[3] = pv[2]
-        pu[0] = pu[0] + obj.rand(-pw, pw, x, y)
-        pu[1] = pu[1] + obj.rand(-pw, pw, x + 1, y)
-        pu[2] = pu[2] + obj.rand(-pw, pw, x + 1, y + 1)
-        pu[3] = pu[3] + obj.rand(-pw, pw, x, y + 1)
-        pv[0] = pv[0] + obj.rand(-ph, ph, x, y + 1000)
-        pv[1] = pv[1] + obj.rand(-ph, ph, x + 1, y + 1000)
-        pv[2] = pv[2] + obj.rand(-ph, ph, x + 1, y + 1 + 1000)
-        pv[3] = pv[3] + obj.rand(-ph, ph, x, y + 1 + 1000)
+        pu[0] = pu[0] + obj.rand(-random_width, random_width, x, y)
+        pu[1] = pu[1] + obj.rand(-random_width, random_width, x + 1, y)
+        pu[2] = pu[2] + obj.rand(-random_width, random_width, x + 1, y + 1)
+        pu[3] = pu[3] + obj.rand(-random_width, random_width, x, y + 1)
+        pv[0] = pv[0] + obj.rand(-random_height, random_height, x, y + 1000)
+        pv[1] = pv[1] + obj.rand(-random_height, random_height, x + 1, y + 1000)
+        pv[2] = pv[2] + obj.rand(-random_height, random_height, x + 1, y + 1 + 1000)
+        pv[3] = pv[3] + obj.rand(-random_height, random_height, x, y + 1 + 1000)
         if x == 0 then
             pu[0] = 0
             pu[3] = 0
-        elseif x == w - 1 then
-            pu[1] = xl
-            pu[2] = xl
+        elseif x == fragment_columns - 1 then
+            pu[1] = width
+            pu[2] = width
         end
         if y == 0 then
             pv[0] = 0
             pv[1] = 0
-        elseif y == h - 1 then
-            pv[2] = yl
-            pv[3] = yl
+        elseif y == fragment_rows - 1 then
+            pv[2] = height
+            pv[3] = height
         end
+
         -- 基準の計算
-        gx = (pu[0] + pu[1] + pu[2] + pu[3]) / 4
-        gy = (pv[0] + pv[1] + pv[2] + pv[3]) / 4
-        gz = 0
-        cx = gx - xl / 2
-        cy = gy - yl / 2
-        cz = 0
-        vx = cx - ax
-        vy = cy - ay
-        vz = -az
-        v = math.sqrt(vx * vx + vy * vy + vz * vz)
+        local base_x = (pu[0] + pu[1] + pu[2] + pu[3]) / 4
+        local base_y = (pv[0] + pv[1] + pv[2] + pv[3]) / 4
+        local draw_x = base_x - width / 2
+        local draw_y = base_y - height / 2
+        local draw_z = 0
+        local velocity_x = draw_x - center_x
+        local velocity_y = draw_y - center_y
+        local velocity_z = -center_z
+        local distance = math.sqrt(velocity_x * velocity_x + velocity_y * velocity_y + velocity_z * velocity_z)
+
         -- 時間の計算
-        t = tm - v / l * delay
-        if t < 0 then
-            t = 0
-            obj.setoption("antialias", 0)
-        else
-            obj.setoption("antialias", 1)
+        local fragment_time = time - distance / diagonal_length * delay_scale
+        if fragment_time < 0 then
+            fragment_time = 0
         end
+
         -- 回転を計算
-        xx = t * obj.rand(-spin, spin, x, y + 2000) / 100
-        yy = t * obj.rand(-spin, spin, x, y + 3000) / 100
-        zz = t * obj.rand(-spin, spin, x, y + 4000) / 100
-        sin_x = math.sin(xx)
-        cos_x = math.cos(xx)
-        sin_y = math.sin(yy)
-        cos_y = math.cos(yy)
-        sin_z = math.sin(zz)
-        cos_z = math.cos(zz)
-        m00 = cos_y * cos_z
-        m01 = -cos_y * sin_z
-        m10 = cos_x * sin_z + sin_x * cos_z * sin_y
-        m11 = cos_x * cos_z - sin_x * sin_z * sin_y
-        m20 = sin_x * sin_z - cos_x * cos_z * sin_y
-        m21 = sin_x * cos_z + cos_x * sin_z * sin_y
+        local rotation_x = fragment_time * obj.rand(-spin_scale, spin_scale, x, y + 2000) / 100
+        local rotation_y = fragment_time * obj.rand(-spin_scale, spin_scale, x, y + 3000) / 100
+        local rotation_z = fragment_time * obj.rand(-spin_scale, spin_scale, x, y + 4000) / 100
+        local sin_x = math.sin(rotation_x)
+        local cos_x = math.cos(rotation_x)
+        local sin_y = math.sin(rotation_y)
+        local cos_y = math.cos(rotation_y)
+        local sin_z = math.sin(rotation_z)
+        local cos_z = math.cos(rotation_z)
+        local matrix_00 = cos_y * cos_z
+        local matrix_01 = -cos_y * sin_z
+        local matrix_10 = cos_x * sin_z + sin_x * cos_z * sin_y
+        local matrix_11 = cos_x * cos_z - sin_x * sin_z * sin_y
+        local matrix_20 = sin_x * sin_z - cos_x * cos_z * sin_y
+        local matrix_21 = sin_x * cos_z + cos_x * sin_z * sin_y
         for i = 0, 3 do
-            xx = pu[i] - gx
-            yy = pv[i] - gy
-            px[i] = m00 * xx + m01 * yy
-            py[i] = m10 * xx + m11 * yy
-            pz[i] = m20 * xx + m21 * yy
+            local vertex_x = pu[i] - base_x
+            local vertex_y = pv[i] - base_y
+            px[i] = matrix_00 * vertex_x + matrix_01 * vertex_y
+            py[i] = matrix_10 * vertex_x + matrix_11 * vertex_y
+            pz[i] = matrix_20 * vertex_x + matrix_21 * vertex_y
         end
+
         -- 表示座標を計算
-        v = 1 / (1 + v * v / (l * l) * impact)
-        vx = vx * v + obj.rand(-size, size, x, y + 4000) * diff
-        vy = vy * v + obj.rand(-size, size, x, y + 5000) * diff
-        vz = vz * v + obj.rand(-size, size, x, y + 6000) * diff
-        cx = cx + t * vx * sp
-        cy = cy + t * vy * sp + t * t * grav
-        cz = cz + t * vz * sp
-        obj.drawpoly(
-            px[0] + cx,
-            py[0] + cy,
-            pz[0] + cz,
-            px[1] + cx,
-            py[1] + cy,
-            pz[1] + cz,
-            px[2] + cx,
-            py[2] + cy,
-            pz[2] + cz,
-            px[3] + cx,
-            py[3] + cy,
-            pz[3] + cz,
+        local distance_rate = 1 / (1 + distance * distance / (diagonal_length * diagonal_length) * impact_scale)
+        velocity_x = velocity_x * distance_rate + obj.rand(-fragment_size, fragment_size, x, y + 4000) * direction_scale
+        velocity_y = velocity_y * distance_rate + obj.rand(-fragment_size, fragment_size, x, y + 5000) * direction_scale
+        velocity_z = velocity_z * distance_rate + obj.rand(-fragment_size, fragment_size, x, y + 6000) * direction_scale
+        draw_x = draw_x + fragment_time * velocity_x * speed_scale
+        draw_y = draw_y
+            + fragment_time * velocity_y * speed_scale
+            + fragment_time * fragment_time * gravity_acceleration
+        draw_z = draw_z + fragment_time * velocity_z * speed_scale
+        vertices[#vertices + 1] = {
+            px[0] + draw_x,
+            py[0] + draw_y,
+            pz[0] + draw_z,
+            px[1] + draw_x,
+            py[1] + draw_y,
+            pz[1] + draw_z,
+            px[2] + draw_x,
+            py[2] + draw_y,
+            pz[2] + draw_z,
+            px[3] + draw_x,
+            py[3] + draw_y,
+            pz[3] + draw_z,
             pu[0],
             pv[0],
             pu[1],
@@ -177,7 +213,9 @@ for y = 0, h - 1 do
             pu[2],
             pv[2],
             pu[3],
-            pv[3]
-        )
+            pv[3],
+        }
     end
 end
+
+obj.drawpoly(vertices)
