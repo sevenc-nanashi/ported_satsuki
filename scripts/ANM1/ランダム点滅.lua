c8ce881b742799fa946f34444d65d7dccc6e2c20
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-17 13:55:15 +0900

    feat: ランダム点滅を移植

--- scripts/ANM1/ランダム点滅.lua
+++ scripts/ANM1/ランダム点滅.lua
@@ -3,17 +3,21 @@
 ---min=0
 ---max=100
 ---step=1
-local track0 = 50
+local threshold = 50
+
 ---$track:透明度
 ---min=0
 ---max=100
-local track1 = 0
+---step=0.1
+local alpha_rate = 0
+
 ---$track:間隔[s]
 ---min=0.01
 ---max=5
 ---step=0.01
-local track2 = 0.01
-a = rand(0, 99, 1, math.floor(obj.time / track2))
-if a >= track0 then
-    obj.alpha = obj.alpha * track1 / 100
+local interval = 0.01
+
+local random_value = obj.rand(0, 99, 1, math.floor(obj.time / interval))
+if random_value >= threshold then
+    obj.alpha = obj.alpha * alpha_rate / 100
 end
