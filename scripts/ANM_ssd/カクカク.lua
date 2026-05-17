75d0a222b0dcd24e757fef63345498ed2a52c993
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-17 15:25:58 +0900

    feat: カクカクを移植

--- scripts/ANM_ssd/カクカク.lua
+++ scripts/ANM_ssd/カクカク.lua
@@ -1,8 +1,6 @@
 --label:${ROOT_CATEGORY}
---param:10
+--param:分割数,10
 local index, ratio = math.modf(obj.getpoint("index"))
-local st = obj.getpoint(index)
-local ed = obj.getpoint(index + 1)
-return st + (ed - st) * math.floor(ratio * obj.getpoint("param")) / obj.getpoint("param")
-
---パラメータの単位は％
+local val_start = obj.getpoint(index)
+local val_end = obj.getpoint(index + 1)
+return val_start + (val_end - val_start) * math.floor(ratio * obj.getpoint("param")) / obj.getpoint("param")
