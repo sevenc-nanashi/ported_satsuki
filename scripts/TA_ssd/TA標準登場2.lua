9b3c51e4d7e73fbd040c390f27550169f59ff390
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-17 11:11:27 +0900

    feat: TA標準登場2を移植

--- scripts/TA_ssd/TA標準登場2.lua
+++ scripts/TA_ssd/TA標準登場2.lua
@@ -3,131 +3,172 @@
 ---min=0
 ---max=5
 ---step=0.01
-local ta = 0.3
+local duration = 0.3
+
 ---$track:イン[s]
 ---min=-1
 ---max=5
 ---step=0.01
-local in_time = 1
+local in_duration = 1
+
 ---$track:アウト[s]
 ---min=-1
 ---max=5
 ---step=0.01
-local out_time = 1
----$track:登場順
+local out_duration = 1
+
+---$select:登場順
+---順番=0
+---逆順=1
+---ランダム順=2
+---ランダム間隔=3
+---内側から=4
+---外側から=5
+local order_mode = 2
+
+---$check:フェード
+local fades = true
+
+---$track:X距離
+---min=-2000
+---max=2000
+---step=0.1
+local distance_x = 0
+
+---$track:Y距離
+---min=-2000
+---max=2000
+---step=0.1
+local distance_y = 0
+
+---$track:Z距離
+---min=-2000
+---max=2000
+---step=0.1
+local distance_z = 0
+
+--trackgroup@distance_x,distance_y,distance_z:距離
+
+---$track:拡大
 ---min=0
+---max=1000
+---step=0.1
+local zoom_rate = 100
+
+---$track:X軸回転
+---min=-720
+---max=720
+---step=0.1
+local rotation_x = 0
+
+---$track:Y軸回転
+---min=-720
+---max=720
+---step=0.1
+local rotation_y = 0
+
+---$track:Z軸回転
+---min=-720
+---max=720
+---step=0.1
+local rotation_z = 0
+
+--trackgroup@rotation_x,rotation_y,rotation_z:回転
+
+---$track:加減速
+---min=1
 ---max=5
 ---step=1
-local jun = 2
----$check:フェード
-local fade = 1
-
----$value:X距離
-local x = 0
-
----$value:Y距離
-local y = 0
-
----$value:Z距離
-local z = 0
-
----$value:拡大
-local s = 100
-
----$value:X軸回転
-local rx = 0
-
----$value:Y軸回転
-local ry = 0
-
----$value:Z軸回転
-local rz = 0
-
----$value:加減速[1-5]
-local beki = 2
+local easing_power = 2
 
 ---$check:アウトは逆方向
-local gyaku = 1
+local reverses_out = true
 
 --インアウトがマイナスの時は最初or最後の中間点でフェードインアウト。数値の時は指定した秒数でフェードインアウト。ゼロのときはフェードインアウトなし。
 
-if jun < 1 then
-    mode = obj.index --順番に登場
-elseif jun < 2 then
-    mode = obj.num - 1 - obj.index --後ろから登場
-elseif jun < 3 then
+if duration == 0 then
+    return
+end
+
+local function get_random_order_index()
     local indexes = {}
     for i = 0, obj.num - 1 do
         indexes[i + 1] = i
     end
     for i = 0, obj.num - 1 do
-        local dest = 0
-        dest = rand(0, obj.num - 1, -obj.num, i + 1)
+        local dest = obj.rand(0, obj.num - 1, -obj.num, i + 1)
         local swap = indexes[i + 1]
         indexes[i + 1] = indexes[dest + 1]
         indexes[dest + 1] = swap
     end
-    mode = indexes[obj.index + 1] --ランダム順に登場
-elseif jun < 4 then
-    mode = rand(0, 100 * (obj.num - 1), obj.index, 0) / 100 --ランダム間隔に登場
-elseif jun < 5 then
-    mode = math.abs((obj.num - 1) / 2 - obj.index) --内側から登場
-else
-    mode = (obj.num - 1) / 2 - math.abs((obj.num - 1) / 2 - obj.index) --外側から登場
+
+    return indexes[obj.index + 1]
 end
 
+local function get_order_index()
+    if order_mode == 0 then
+        return obj.index --順番に登場
+    elseif order_mode == 1 then
+        return obj.num - 1 - obj.index --後ろから登場
+    elseif order_mode == 2 then
+        return get_random_order_index() --ランダム順に登場
+    elseif order_mode == 3 then
+        return obj.rand(0, 100 * (obj.num - 1), obj.index, 0) / 100 --ランダム間隔に登場
+    elseif order_mode == 4 then
+        return math.abs((obj.num - 1) / 2 - obj.index) --内側から登場
+    end
+
+    return (obj.num - 1) / 2 - math.abs((obj.num - 1) / 2 - obj.index) --外側から登場
+end
+
+local function apply_transform(progress, direction)
+    if progress <= 0 then
+        return
+    end
+
+    if progress > 1 then
+        obj.alpha = 0
+        progress = 1
+    end
+    progress = progress ^ easing_power
+
+    obj.ox = obj.ox + distance_x * progress * direction
+    obj.oy = obj.oy + distance_y * progress * direction
+    obj.oz = obj.oz + distance_z * progress * direction
+    obj.rx = obj.rx + rotation_x * progress * direction
+    obj.ry = obj.ry + rotation_y * progress * direction
+    obj.rz = obj.rz + rotation_z * progress * direction
+    obj.zoom = obj.zoom + progress * (zoom_rate - 100) / 100
+    if fades then
+        obj.alpha = obj.alpha * (1 - progress)
+    end
+end
+
+local order_index = get_order_index()
+
 --シーンイン
-if in_time ~= 0 then
-    if in_time < 0 then
-        in_time = obj.getvalue("time", 0, 1)
-    else
+if in_duration ~= 0 then
+    if in_duration < 0 then
+        in_duration = obj.getvalue("time", 0, 1)
     end
 
-    local i = (ta - obj.time + mode * in_time / obj.num) / ta
-
-    if i > 0 then
-        if i > 1 then
-            obj.alpha = 0
-            i = 1
-        end
-        i = i ^ beki
-        obj.ox = obj.ox + x * i
-        obj.oy = obj.oy + y * i
-        obj.oz = obj.oz + z * i
-        obj.rx = obj.rx + rx * i
-        obj.ry = obj.ry + ry * i
-        obj.rz = obj.rz + rz * i
-        obj.zoom = obj.zoom + i * (s - 100) / 100
-        if fade == 1 then
-            obj.alpha = obj.alpha * (1 - i)
-        end
-    end
+    local progress = (duration - obj.time + order_index * in_duration / obj.num) / duration
+    apply_transform(progress, 1)
 end
 
 --シーンアウト
-if out_time ~= 0 then
-    if out_time < 0 then
-        out_time = obj.totaltime - obj.getvalue("time", 0, obj.getoption("section_num") - 1)
-    else
+if out_duration ~= 0 then
+    if out_duration < 0 then
+        out_duration = obj.totaltime - obj.getvalue("time", 0, obj.getoption("section_num") - 1)
     end
 
-    local r = -(-ta - obj.num * out_time / obj.num - obj.time + obj.totaltime + mode * out_time / obj.num) / ta
-
-    if r > 0 then
-        if r > 1 then
-            obj.alpha = 0
-            r = 1
-        end
-        r = r ^ beki
-        obj.ox = obj.ox + x * r * (1 - 2 * gyaku)
-        obj.oy = obj.oy + y * r * (1 - 2 * gyaku)
-        obj.oz = obj.oz + z * r * (1 - 2 * gyaku)
-        obj.rx = obj.rx + rx * r * (1 - 2 * gyaku)
-        obj.ry = obj.ry + ry * r * (1 - 2 * gyaku)
-        obj.rz = obj.rz + rz * r * (1 - 2 * gyaku)
-        obj.zoom = obj.zoom + r * (s - 100) / 100
-        if fade == 1 then
-            obj.alpha = obj.alpha * (1 - r)
-        end
-    end
+    local progress = -(
+            -duration
+            - obj.num * out_duration / obj.num
+            - obj.time
+            + obj.totaltime
+            + order_index * out_duration / obj.num
+        ) / duration
+    local out_direction = reverses_out and -1 or 1
+    apply_transform(progress, out_direction)
 end
