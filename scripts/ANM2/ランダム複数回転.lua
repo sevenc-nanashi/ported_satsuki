f23089e6b42ee7a0f01f5e54cf9148e206eaf56f
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-05-17 13:58:16 +0900

    feat: ランダム複数回転を移植

--- scripts/ANM2/ランダム複数回転.lua
+++ scripts/ANM2/ランダム複数回転.lua
@@ -3,25 +3,33 @@
 ---min=1
 ---max=50
 ---step=1
-local n = 5
+local count = 5
+
 ---$track:回転速度
 ---min=0
 ---max=800
-local track1 = 100
+---step=0.1
+local rotation_speed = 100
+
 ---$track:回転範囲
 ---min=0
 ---max=800
-local rv = 100
+---step=0.1
+local rotation_range = 100
+
 ---$track:拡大範囲
 ---min=0
 ---max=100
-local ms = 100
+---step=0.1
+local min_zoom_rate = 100
+
 obj.effect()
-v = track1 / 100
-for i = 0, n - 1 do
-    s = obj.rand(ms, 100, i, 6) / 100
-    rx = obj.rand(0, 360, i, 0) + obj.rand(-rv, rv, i, 3) * obj.time * v
-    ry = obj.rand(0, 360, i, 1) + obj.rand(-rv, rv, i, 4) * obj.time * v
-    rz = obj.rand(0, 360, i, 2) + obj.rand(-rv, rv, i, 5) * obj.time * v
-    obj.draw(0, 0, 0, s, 1, rx, ry, rz)
+
+local velocity = rotation_speed / 100
+for i = 0, count - 1 do
+    local zoom = obj.rand(min_zoom_rate, 100, i, 6) / 100
+    local rotation_x = obj.rand(0, 360, i, 0) + obj.rand(-rotation_range, rotation_range, i, 3) * obj.time * velocity
+    local rotation_y = obj.rand(0, 360, i, 1) + obj.rand(-rotation_range, rotation_range, i, 4) * obj.time * velocity
+    local rotation_z = obj.rand(0, 360, i, 2) + obj.rand(-rotation_range, rotation_range, i, 5) * obj.time * velocity
+    obj.draw(0, 0, 0, zoom, 1, rotation_x, rotation_y, rotation_z)
 end
